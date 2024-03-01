d7c7e6b658ecae5c5f4a38372bbed3a58e729ad6
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-01 09:04:46 +0100

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+
+// Styles
+import "bootstrap/dist/css/bootstrap.css";
+
+// Pages
+import App from "./App";
+
+const domNode = document.getElementById("root");
+const root = createRoot(domNode);
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+);
+
+serviceWorker.unregister();
