6dda85b224fa2f1733bab422f93d9c092e938f64
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-01 12:41:31 +0100

    Add files via upload

---
+++ src/components/layouts/Auth.js
@@ -0,0 +1,32 @@
+import { useEffect } from "react";
+import { Outlet } from "react-router-dom";
+import { Container, Row, Col, Card } from "reactstrap";
+
+export default function Auth(props) {
+  useEffect(() => {
+    document.body.classList.add("register-page");
+	return function cleanup() {
+      document.body.classList.remove("register-page");
+    };
+  });
+  
+  return (
+    <>
+	  <div
+        className="page-header"
+        style={{ backgroundImage: "url(" + require("../../assets/img/login-image.jpg") + ")" }}
+      >
+		<div className="filter" />
+		<Container>
+		  <Row>
+		    <Col>
+			  <Card className="card-register ml-auto mr-auto">
+	            <Outlet />
+			  </Card>
+			</Col>
+		  </Row>
+		</Container>
+	  </div>
+    </>
+  );
+}
---
+++ src/components/views/auth/Login.js
@@ -0,0 +1,8 @@
+export default function Login(props) {  
+  return (
+    <>
+      <h4 className="font-weight-bold">Sign In</h4>
+	  <p className="mb-0">Enter your email and password to sign in</p>
+    </>
+  );
+}
