8c5096d407b57d93e3b2f62fb6ad05a1f892aa2f
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-01 11:44:17 +0100

    Update LocalStorage.js

--- src/utils/LocalStorage.js
+++ src/utils/LocalStorage.js
@@ -6,14 +6,13 @@ export default function useLocalStorage(key) {
         const storedValue = window.localStorage.getItem(key);
         return storedValue ? JSON.parse(storedValue) : undefined;
       } catch (error) {
-        console.error(error);
         return undefined;
       }
     });
   
     useEffect(() => {
       try {
-        window.localStorage.setItem(key, value ? JSON.stringify(value) : undefined);
+        window.localStorage.setItem(key, JSON.stringify(value));
       } catch (error) {
         console.error(error);
       }
