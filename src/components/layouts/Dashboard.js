49e075d142f038513061e20051c92f3f3f6572fa
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-04 17:07:12 +0100

    Add files via upload

--- src/components/layouts/Dashboard.js
+++ src/components/layouts/Dashboard.js
@@ -12,10 +12,10 @@ export default function Dashboard(props) {
         <Container fluid className="py-4">
           <Navbar expand="lg" variant="main" className="px-0 mx-4 shadow-none border-radius-xl" id="navbarBlur" data-scroll="true">
             <Container fluid className="py-1 px-3">
-              <Breadcrumb className="bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
+              <Breadcrumb listProps={{ className="bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5" }}>
                 <Breadcrumb.Item href="/" className="text-sm opacity-5 text-dark">Pages</Breadcrumb.Item>
                 <Breadcrumb.Item href="/test" className="text-sm text-dark" active>Dashboard</Breadcrumb.Item>
-                <h6 class="font-weight-bolder mb-0">Dashboard</h6>
+                <Breadcrumb.Item as="h6" className="font-weight-bolder mb-0">Dashboard</Breadcrumb.Item>
 			  </Breadcrumb>
               <Navbar.Collapse id="navbar" className="mt-sm-0 mt-2 me-md-0 me-sm-4">
                 <Nav className="justify-content-end">
