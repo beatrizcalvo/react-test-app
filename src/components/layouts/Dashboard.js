222772105c2181b0733122e9295c30c4badade46
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-04 18:09:06 +0100

    Add files via upload

--- src/components/layouts/Dashboard.js
+++ src/components/layouts/Dashboard.js
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { Container, Navbar, Nav, Breadcrumb } from "react-bootstrap";
+import { Container, Row, Navbar, Nav, Breadcrumb } from "react-bootstrap";
 
 export default function Dashboard(props) {
   useEffect(() => {
@@ -12,11 +12,17 @@ export default function Dashboard(props) {
         <Container fluid className="py-4">
           <Navbar expand="lg" variant="main" className="px-0 mx-4 shadow-none border-radius-xl" id="navbarBlur" data-scroll="true">
             <Container fluid className="py-1 px-3">
-              <Breadcrumb listProps={{ className: 'bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5' }}>
-                <Breadcrumb.Item href="/" className="text-sm" linkProps={{ className: 'opacity-5 text-dark' }}>Pages</Breadcrumb.Item>
-                <Breadcrumb.Item href="/test" className="text-sm text-dark" active>Dashboard</Breadcrumb.Item>
-                <Breadcrumb.Item as="h6">Dashboard</Breadcrumb.Item>
-			  </Breadcrumb>
+              <Container>
+                <Row>
+                  <Breadcrumb listProps={{ className: 'bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5' }}>
+                    <Breadcrumb.Item href="/" className="text-sm" linkProps={{ className: 'opacity-5 text-dark' }}>Pages</Breadcrumb.Item>
+                    <Breadcrumb.Item href="/test" className="text-sm text-dark" active>Dashboard</Breadcrumb.Item>
+			      </Breadcrumb>
+                </Row>
+                <Row>
+                  <h6 className="font-weight-bolder mb-0">Dashboard</h6>
+                </Row>
+              </Container>
               <Navbar.Collapse id="navbar" className="mt-sm-0 mt-2 me-md-0 me-sm-4">
                 <div className="ms-md-auto pe-md-3 d-flex align-items-center" />
                 <Nav className="justify-content-end">
