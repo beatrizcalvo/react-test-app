fb1bc23eb5c5486cda218d4260362f65d9d90ccc
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-04 14:29:46 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 import useLocalStorage from "./utils/LocalStorage";
 import ProtectedRoute from "./utils/ProtectedRoute";
@@ -25,6 +25,7 @@ export default function App(props) {
         <Route path="login" element={<Login />} />
         <Route path="register" element={<Register />} />
       </Route>
+	  <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   );
 }
