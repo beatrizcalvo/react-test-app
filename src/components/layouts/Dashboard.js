0ac6d2ef66855ee818a0c453a6dc935a30c896cf
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-04 12:13:31 +0100

    Add files via upload

--- src/components/layouts/Dashboard.js
+++ src/components/layouts/Dashboard.js
@@ -12,7 +12,10 @@ export default function Dashboard(props) {
         <Container fluid className="py-4">
           <Navbar expand="lg">
             <Container fluid className="py-1 px-3">
-              <Nav role="breadcrumb>
+              <Nav role="breadcrumb">
+                <Nav.Item>
+                  <Nav.Link href="/home">Active</Nav.Link>
+                </Nav.Item>
                 <h6 class="font-weight-bolder mb-0">Dashboard</h6>
               </Nav>
             </Container>
