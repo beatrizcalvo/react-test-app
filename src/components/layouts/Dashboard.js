0d5318f373dafe8e2e32cfa511f02ec74d48f8ec
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-04 21:52:59 +0100

    Update Dashboard.js

--- src/components/layouts/Dashboard.js
+++ src/components/layouts/Dashboard.js
@@ -10,46 +10,35 @@ export default function Dashboard(props) {
     <>
       <main className="main-content position-relative max-height-vh-100 h-100 border-radius-lg ps ps--active-y">
         <Container fluid className="py-4">
-          <Navbar expand="lg" variant="main" className="px-0 mx-4 shadow-none border-radius-xl" id="navbarBlur" data-scroll="true">
-            <Container fluid className="py-1 px-3">
-              <div className="col-md-auto">
-                <Row>
-                  <Breadcrumb listProps={{ className: 'bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5' }}>
-                    <Breadcrumb.Item href="/" className="text-sm" linkProps={{ className: 'opacity-5 text-dark' }}>Pages</Breadcrumb.Item>
-                    <Breadcrumb.Item href="/test" className="text-sm text-dark" active>Dashboard</Breadcrumb.Item>
-	          </Breadcrumb>
-                </Row>
-                <Row>
-                  <h6 className="font-weight-bolder mb-0">Dashboard</h6>
-                </Row>
-              </div>
-              <Navbar.Collapse id="navbar" className="mt-sm-0 mt-2 me-md-0 me-sm-4">
-                <div className="ms-md-auto pe-md-3 d-flex align-items-center" />
-                <Nav className="justify-content-end">
-                  <Nav.Item>
-                    <Nav.Link href="/profile" className="text-body p-0">
-                      <i className="fa-solid fa-circle-user me-sm-1"/>
-                    </Nav.Link>
-                  </Nav.Item>
-                  <Nav.Item className="px-2">
-                    <Nav.Link href="/profile" className="text-body p-0">
-                      <i className="fa-solid fa-bars me-sm-1"/>
-                    </Nav.Link>
-                  </Nav.Item>
-	          <Nav.Item className="px-2">
-	            <Nav.Link href="/settings" className="text-body p-0">
-	              <i className="fa-solid fa-gear me-sm-1"/>
-	            </Nav.Link>
-	          </Nav.Item>
-	          <Nav.Item>
-	            <Nav.Link className="text-body p-0">
-	              <i className="fa-solid fa-right-from-bracket"/>
-	            </Nav.Link>
-	          </Nav.Item>
-                </Nav>
-              </Navbar.Collapse>
-            </Container>
-          </Navbar>
+          <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
+	    <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <NavDropdown title="Dropdown" id="collapsible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#deets">More deets</Nav.Link>
+            <Nav.Link eventKey={2} href="#memes">
+              Dank memes
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+	  </Navbar>
         </Container>
       </main>
     </>
