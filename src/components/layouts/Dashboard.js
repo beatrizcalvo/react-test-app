302f399222154a0b0ab381c079a72ed7dcae6585
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-04 16:39:11 +0100

    Add files via upload

--- src/components/layouts/Dashboard.js
+++ src/components/layouts/Dashboard.js
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { Container, Navbar, Breadcrumb } from "react-bootstrap";
+import { Container, Navbar, Nav, Breadcrumb } from "react-bootstrap";
 
 export default function Dashboard(props) {
   useEffect(() => {
@@ -13,9 +13,16 @@ export default function Dashboard(props) {
           <Navbar expand="lg" variant="main" className="px-0 mx-4 shadow-none border-radius-xl" id="navbarBlur" data-scroll="true">
             <Container fluid className="py-1 px-3">
               <Breadcrumb className="bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
-                <Breadcrumb.Item href="/" className="text-sm">Pages</Breadcrumb.Item>
-                <Breadcrumb.Item href="/test" active>Dashboard</Breadcrumb.Item>
+                <Breadcrumb.Item href="/" className="text-sm opacity-5 text-dark">Pages</Breadcrumb.Item>
+                <Breadcrumb.Item href="/test" className="text-sm text-dark" active>Dashboard</Breadcrumb.Item>
+                <h6 class="font-weight-bolder mb-0">Dashboard</h6>
 			  </Breadcrumb>
+              <Navbar.Collapse id="navbar" className="mt-sm-0 mt-2 me-md-0 me-sm-4">
+                <Nav className="justify-content-end">
+                  <Nav.Item className="d-flex align-items-center">
+                  </Nav.Item>
+                </Nav>
+              </Navbar>
             </Container>
           </Navbar>
         </Container>
