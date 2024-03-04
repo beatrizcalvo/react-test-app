4ca8922dedcbf34e0badbdf095c89654f64ce0b4
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-04 18:27:01 +0100

    Add files via upload

--- src/components/layouts/Dashboard.js
+++ src/components/layouts/Dashboard.js
@@ -12,7 +12,7 @@ export default function Dashboard(props) {
         <Container fluid className="py-4">
           <Navbar expand="lg" variant="main" className="px-0 mx-4 shadow-none border-radius-xl" id="navbarBlur" data-scroll="true">
             <Container fluid className="py-1 px-3">
-              <Container>
+              <div className="col-md-auto">
                 <Row>
                   <Breadcrumb listProps={{ className: 'bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5' }}>
                     <Breadcrumb.Item href="/" className="text-sm" linkProps={{ className: 'opacity-5 text-dark' }}>Pages</Breadcrumb.Item>
@@ -22,16 +22,19 @@ export default function Dashboard(props) {
                 <Row>
                   <h6 className="font-weight-bolder mb-0">Dashboard</h6>
                 </Row>
-              </Container>
+              </div>
               <Navbar.Collapse id="navbar" className="mt-sm-0 mt-2 me-md-0 me-sm-4">
                 <div className="ms-md-auto pe-md-3 d-flex align-items-center" />
                 <Nav className="justify-content-end">
                   <Nav.Item>
-                    <Nav.Link href="/profile" className="text-body p-0 position-relative">
-                      <i className="fa-solid fa-circle-user material-icons me-sm-1"/>
+                    <Nav.Link href="/profile" className="text-body p-0">
+                      <i className="fa-solid fa-circle-user me-sm-1"/>
                     </Nav.Link>
                   </Nav.Item>
-                  <Nav.Item className="d-xl-none ps-3 d-flex align-items-center">
+                  <Nav.Item className="px-3">
+                    <Nav.Link href="/profile" className="text-body p-0">
+                      <i className="fa-solid fa-bars me-sm-1"/>
+                    </Nav.Link>
                   </Nav.Item>
                 </Nav>
               </Navbar.Collapse>
