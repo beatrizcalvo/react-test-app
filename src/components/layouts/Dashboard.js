19cf612ac61d0b031f08a2881b8bddd7391cb3e5
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-04 14:15:59 +0100

    Add files via upload

--- src/components/layouts/Dashboard.js
+++ src/components/layouts/Dashboard.js
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { Container, Navbar, Nav } from "react-bootstrap";
+import { Container, Navbar, Breadcrumb } from "react-bootstrap";
 
 export default function Dashboard(props) {
   useEffect(() => {
@@ -12,17 +12,13 @@ export default function Dashboard(props) {
         <Container fluid className="py-4">
           <Navbar expand="lg" variant="main" className="px-0 mx-4 shadow-none border-radius-xl" id="navbarBlur" data-scroll="true">
             <Container fluid className="py-1 px-3">
-              <nav aria-label="breadcrumb">
-                <Nav bsPrefix="breadcrumb">
-                  <Nav.Item>
-                    <Nav.Link href="/home">Active</Nav.Link>
-                  </Nav.Item>
-                  <Nav.Item>
-                    <Nav.Link eventKey="link-1">Link</Nav.Link>
-                  </Nav.Item>				  
-                </Nav>
-                <h6 class="font-weight-bolder mb-0">Dashboard</h6>
-              </nav>			  
+              <Breadcrumb>
+                <Breadcrumb.Item href="#">Home</Breadcrumb.Item>
+                <Breadcrumb.Item href="https://getbootstrap.com/docs/4.0/components/breadcrumb/">
+                  Library
+                </Breadcrumb.Item>
+                <Breadcrumb.Item active>Data</Breadcrumb.Item>
+			  </Breadcrumb>
             </Container>
           </Navbar>
         </Container>
