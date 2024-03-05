9817001220d78e450fe0b4d8de42995ce9df4c42
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-05 10:17:35 +0100

    Add files via upload

--- src/components/layouts/Dashboard.js
+++ src/components/layouts/Dashboard.js
@@ -1,35 +1,12 @@
-import { useEffect, useRef } from "react";
-import { useLocation } from "react-router-dom";
-import PerfectScrollbar from "perfect-scrollbar";
+import { useEffect } from "react";
 
 import NavbarDashboard from "../navbars/NavbarDashboard";
 
-var ps;
-
 export default function Dashboard(props) {
-  const mainPanel = useRef();
-  const location = useLocation();
 
   useEffect(() => {
     document.body.classList.add("bg-gray-200");
-
-    if (navigator.platform.indexOf("Win") > -1) {
-      ps = new PerfectScrollbar(mainPanel.current);
-      document.body.classList.toggle("perfect-scrollbar-on");
-    }
-
-    return function cleanup() {
-      if (navigator.platform.indexOf("Win") > -1) {
-        ps.destroy();
-        document.body.classList.toggle("perfect-scrollbar-on");
-      }
-    }
   });
-
-  useEffect(() => {
-    mainPanel.current.scrollTop = 0;
-    document.scrollingElement.scrollTop = 0;
-  }, [location]);
   
   return (
     <>
--- src/components/navbars/NavbarDashboard.js
+++ src/components/navbars/NavbarDashboard.js
@@ -1,10 +1,22 @@
+import { useState } from "react";
+import { useLocation } from "react-router-dom";
 import { Container, Navbar } from "react-bootstrap";
 
 export default function NavbarDashboard(props) {
+  const [color, setColor] = useState("transparent");
+  const location = useLocation();
+
   return (
     <>
       <Container fluid>
-        <Navbar collapseOnSelect expand="lg" variant="transparent" className="shadow-none">
+        <Navbar 
+          collapseOnSelect 
+          expand="lg" 
+          variant={color} 
+          className="shadow-none"
+        >
+          <Container>
+          </Container>
         </Navbar>
       </Container>
     </>
