990f52356e3b3e2b0c3fbbdb1f80654f84677745
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-05 13:08:23 +0100

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import "./assets/css/material-ui.css";
 import "perfect-scrollbar/css/perfect-scrollbar.css";
 
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
 
 const domNode = document.getElementById("root");
 const root = createRoot(domNode);
