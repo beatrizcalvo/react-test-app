aa6d7bbb985fe9006d1f739b13ca29f9a73b8b63
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-05 12:33:56 +0100

    Add files via upload

--- src/components/breadcrumbs/BreadcrumbsDashboard.js
+++ src/components/breadcrumbs/BreadcrumbsDashboard.js
@@ -1,5 +1,6 @@
+import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
-import { Container, Row, Breadcrumb } from "react-bootstrap";
+import { Col, Row, Breadcrumb } from "react-bootstrap";
 
 export default function BreadcrumbsDashboard(props) {
   const location = useLocation();
@@ -8,14 +9,23 @@ export default function BreadcrumbsDashboard(props) {
     let brandName = "Dashboard";
     return brandName;
   };
+  
+  useEffect(() => {
+    console.log(location.pathname.split("/");
+  }, [location]);
 
   return (
     <>
-      <Container>
+      <Col>
+        <Row>
+          <Breadcrumb>
+            <Breadcrumb.Item href="/" className="text-sm">Home</Breadcrumb.Item>
+          </Breadcrumb>
+        </Row>
         <Row>
           <h6 className="font-weight-bolder mb-0">{getBrand()}</h6>
         </Row>
-      </Container>
+      </Col>
     </>
   );
 }
