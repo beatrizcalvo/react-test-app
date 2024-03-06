3c5d449bfad5ea27124b1c327582817e0ebb1ec6
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-06 11:14:57 +0100

    Add files via upload

--- src/components/navbars/NavbarDashboard.js
+++ src/components/navbars/NavbarDashboard.js
@@ -34,16 +34,14 @@ export default function NavbarDashboard(props) {
 		  >
             <div className="ms-md-auto pe-md-3 d-flex align-items-center" />
             <Nav className="justify-content-end">
-              <Nav.Item className="d-flex align-items-center">
-                <Nav.Link href="/profile" 
-                  className="text-body"
-                >
-                  <i className="fa-solid fa-user-circle" />
-                  <span className="text-uppercase d-lg-none d-md-block">
-                    Profile
-                  </span>
-                </Nav.Link>
-              </Nav.Item>
+              <Nav.Link href="/profile" 
+                className="d-flex align-items-center icom-md text-body"
+              >
+                <i className="fa-solid fa-user-circle" />
+                <span className="text-uppercase d-lg-none d-md-block">
+                  Profile
+                </span>
+              </Nav.Link>
               <Nav.Link href="#pricing">Pricing</Nav.Link>
             </Nav>
           </Navbar.Collapse>
