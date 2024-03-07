0a42348e9ff05490b72695f0575ca94a898254a2
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-07 09:58:38 +0100

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import "./assets/css/material-ui.css";
 import "perfect-scrollbar/css/perfect-scrollbar.css";
+import "./assets/css/app.css";
 
 import App from "./App";
 
