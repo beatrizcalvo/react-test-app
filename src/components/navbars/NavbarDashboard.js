1569e2f3e5b9873a52924afa864ca1c4afda73fa
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-08 08:40:00 +0100

    Add files via upload

--- src/components/navbars/NavbarDashboard.js
+++ src/components/navbars/NavbarDashboard.js
@@ -42,21 +42,7 @@ export default function NavbarDashboard(props) {
     setColor((isOpen) ? "transparent" : "dark");
     setIsOpen(!isOpen);
   };
-  
-  // function that adds color dark/transparent to the navbar on resize (this is for the collapse)
-  const updateColor = () => {
-    console.log(updateColor);
-    if (window.innerWidth < 993 && isOpen) {
-      setColor("dark");
-    } else {
-      setColor("transparent");
-    }
-  };
-  
-  useEffect(() => {
-    window.addEventListener("resize", updateColor.bind(this));
-  });
-  
+    
   // Add a tooltip in a link
   const LinkTooltip = ({ id, title, children }) => (
     <OverlayTrigger placement="bottom" overlay={<Tooltip id={id}>{title}</Tooltip>}>
