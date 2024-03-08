a2e67da72507e2787ce273d32331b254c9c4a180
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-08 12:13:51 +0100

    Add files via upload

--- src/components/navbars/NavbarDashboard.js
+++ src/components/navbars/NavbarDashboard.js
@@ -44,8 +44,13 @@ const NavbarDashboard = forwardRef(({ handleRegister }, _ref) => {
 
   // Functions to update state fields from parent
   useImperativeHandle(_ref, () => ({
-    updateIsBlur: (value) => {
-      setIsBlur(value);
+    showBlur: () => {
+      setIsOpen(false);
+      setIsBlur(true);
+    };
+    hideBlur: () => {
+      setIsOpen(false);
+      setIsBlur(false);
     }
   }));
 
