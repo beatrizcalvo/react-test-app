d90dd63abe2c738a74596b3d2403e35b6e47a004
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-12 10:59:52 +0100

    Add files via upload

--- src/components/sidebars/SidebarDashboard.js
+++ src/components/sidebars/SidebarDashboard.js
@@ -9,6 +9,10 @@ export default function SidebarDashboard(props) {
         variant="vertical"
         className="sidenav border-0 border-radius-xl my-3 fixed-start ms-3 bg-gradient-dark"
       >
+        <div class="sidenav-header">
+
+        </div>
+        <hr class="horizontal light mt-0 mb-2">
       </Navbar>
     </>
   );
