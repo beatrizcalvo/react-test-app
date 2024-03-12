baadd38b09fd653236f1c219bfed868f853233ea
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-12 16:44:35 +0100

    Update SidebarDashboard.js

--- src/components/sidebars/SidebarDashboard.js
+++ src/components/sidebars/SidebarDashboard.js
@@ -9,8 +9,12 @@ export default function SidebarDashboard(props) {
         variant="vertical"
         className="sidenav border-0 border-radius-xl my-3 fixed-start ms-3 bg-gradient-dark"
       >
-        <SidebarMenu.Header bsPrefix="sidenav">
-          <SidebarMenu.Brand>
+        <SidebarMenu.Header bsPrefix="sidenav-header">
+          <SidebarMenu.Brand
+            as="a" 
+            bsPrefix="navbar-brand" 
+            className="m-0"
+          >
             <img src={require("../../assets/img/logo-ct.png")} className="navbar-brand-img h-100" />
             <span className="ms-1 font-weight-bold text-white">
               React Test App
@@ -18,6 +22,8 @@ export default function SidebarDashboard(props) {
           </SidebarMenu.Brand>
         </SidebarMenu.Header>
         <hr class="horizontal light mt-0 mb-2" />
+        <SidebarMenu.Body>
+        </SidebarMenu.Body>
       </SidebarMenu>
     </>
   );
