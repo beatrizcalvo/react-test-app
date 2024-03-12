2ecd423d1c82e381de57abcfd01560c24bb59d7f
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-12 16:30:05 +0100

    Update SidebarDashboard.js

--- src/components/sidebars/SidebarDashboard.js
+++ src/components/sidebars/SidebarDashboard.js
@@ -3,7 +3,11 @@ import { SidebarMenu } from 'react-bootstrap-sidebar-menu';
 export default function SidebarDashboard(props) {
   return (
     <>
-      
+      <SidebarMenu
+        expand="xs"
+        variant="vertical"
+      >
+      </SidebarMenu>
     </>
   );
 };
