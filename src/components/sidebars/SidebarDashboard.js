14699b0e2abc1a9c625b9a3ea63dd49b1a7096fa
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-12 16:59:54 +0100

    Update SidebarDashboard.js

--- src/components/sidebars/SidebarDashboard.js
+++ src/components/sidebars/SidebarDashboard.js
@@ -1,5 +1,27 @@
 import SidebarMenu from 'react-bootstrap-sidebar-menu';
 
+function SidebarHeader(props) {
+  return (
+    <>
+      <SidebarMenu.Header 
+        bsPrefix="sidenav-header"
+        className="d-flex align-items-center justify-content-center"
+      >
+        <SidebarMenu.Brand
+          as="a" 
+          bsPrefix="navbar-brand" 
+          className="m-0"
+        >
+          <img src={require("../../assets/img/logo-ct.png")} className="navbar-brand-img h-100" />
+          <span className="ms-1 font-weight-bold text-white">
+            React Test App
+          </span>
+        </SidebarMenu.Brand>
+      </SidebarMenu.Header>
+    </>
+  );
+};
+
 export default function SidebarDashboard(props) {
   return (
     <>
@@ -9,28 +31,17 @@ export default function SidebarDashboard(props) {
         variant="vertical"
         className="sidenav border-0 border-radius-xl my-3 fixed-start ms-3 bg-gradient-dark"
       >
-        <SidebarMenu.Header 
-          bsPrefix="sidenav-header"
-          className="d-flex align-items-center justify-content-center"
-        >
-          <SidebarMenu.Brand
-            as="a" 
-            bsPrefix="navbar-brand" 
-            className="m-0"
-          >
-            <img src={require("../../assets/img/logo-ct.png")} className="navbar-brand-img h-100" />
-            <span className="ms-1 font-weight-bold text-white">
-              React Test App
-            </span>
-          </SidebarMenu.Brand>
-        </SidebarMenu.Header>
+        <SidebarHeader />
         <hr class="horizontal light mt-0 mb-2" />
         <SidebarMenu.Collapse 
           bsPrefix="navbar-collapse"
           className="w-auto"
         >
-          <SidebarMenu.Sub>
-          </SidebarMenu.Sub>
+          <SidebarMenu.Nav
+            as="ul"
+            bsPrefix="navbar-nav"
+          >
+          </SidebarMenu.Nav>
           <hr class="horizontal light mt-0" />
         </SidebarMenu.Collapse>
       </SidebarMenu>
