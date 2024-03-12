37b68b4f4c495eac63fc57166a6868840abbe23f
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-12 12:41:36 +0100

    Update SidebarDashboard.js

--- src/components/sidebars/SidebarDashboard.js
+++ src/components/sidebars/SidebarDashboard.js
@@ -1,4 +1,4 @@
-import { Navbar, NavbarBrand, Nav } from "react-bootstrap";
+import { Container, Navbar, NavbarBrand, Nav } from "react-bootstrap";
 
 export default function SidebarDashboard(props) {
   return (
@@ -9,24 +9,26 @@ export default function SidebarDashboard(props) {
         variant="vertical"
         className="sidenav border-0 border-radius-xl my-3 fixed-start ms-3 bg-gradient-dark"
       >
-        <div class="sidenav-header">
-          <i class="fas fa-times p-3 cursor-pointer text-white opacity-5 position-absolute end-0 top-0 d-none d-xl-none" />
-          <NavbarBrand className="m-0">
-            <img src={require("../../assets/img/logo-ct.png")} className="navbar-brand-img h-100" />
-            <span className="ms-1 font-weight-bold text-white">
-              React Test App
-            </span>
-          </NavbarBrand>
-        </div>
-        <hr class="horizontal light mt-0 mb-2" />
-        <Navbar.Collapse 
-          id="sidebar-dashboard-collapse" 
-          className="w-auto"
-        >
-          <Nav>
+        <Container fluid>
+          <div class="sidenav-header">
+            <i class="fas fa-times p-3 cursor-pointer text-white opacity-5 position-absolute end-0 top-0 d-none d-xl-none" />
+            <NavbarBrand className="m-0">
+              <img src={require("../../assets/img/logo-ct.png")} className="navbar-brand-img h-100" />
+              <span className="ms-1 font-weight-bold text-white">
+                React Test App
+              </span>
+            </NavbarBrand>
+          </div>
+          <hr class="horizontal light mt-0 mb-2" />
+          <Navbar.Collapse 
+            id="sidebar-dashboard-collapse" 
+            className="w-auto"
+          >
+            <Nav>
             
-          </Nav>
-        </Navbar.Collapse>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
       </Navbar>
     </>
   );
