8fb9023a1bbdab76db0ab18345c0a5e61931f108
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-12 11:23:08 +0100

    Update SidebarDashboard.js

--- src/components/sidebars/SidebarDashboard.js
+++ src/components/sidebars/SidebarDashboard.js
@@ -1,4 +1,4 @@
-import { Navbar } from "react-bootstrap";
+import { Navbar, NavbarBrand } from "react-bootstrap";
 
 export default function SidebarDashboard(props) {
   return (
@@ -10,7 +10,9 @@ export default function SidebarDashboard(props) {
         className="sidenav border-0 border-radius-xl my-3 fixed-start ms-3 bg-gradient-dark"
       >
         <div class="sidenav-header">
-
+          <i class="fas fa-times p-3 cursor-pointer text-white opacity-5 position-absolute end-0 top-0 d-none d-xl-none" />
+          <NavbarBrand className="m-0">
+          </NavbarBrand>
         </div>
         <hr class="horizontal light mt-0 mb-2" />
       </Navbar>
