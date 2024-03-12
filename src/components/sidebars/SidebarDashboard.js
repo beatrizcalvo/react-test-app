030bd93bbfbc67907a8749e5c914d03bf77d24d9
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-12 17:48:08 +0100

    Update SidebarDashboard.js

--- src/components/sidebars/SidebarDashboard.js
+++ src/components/sidebars/SidebarDashboard.js
@@ -43,25 +43,14 @@ export default function SidebarDashboard(props) {
   
   return (
     <>
-      <SidebarMenu
-        bsPrefix="navbar" 
-        expand="xs"
-        variant="vertical"
-        className="sidenav border-0 border-radius-xl my-3 fixed-start ms-3 bg-gradient-dark"
-      >
+      <SidebarMenu bsPrefix="navbar" expand="xs" variant="vertical" className="sidenav border-0 border-radius-xl my-3 fixed-start ms-3 bg-gradient-dark">
         <SidebarHeader />
         <hr class="horizontal light mt-0 mb-2" />
-        <SidebarMenu.Collapse 
-          bsPrefix="navbar-collapse"
-          className="w-auto"
-        >
-          <SidebarMenu.Nav
-            as="ul"
-            bsPrefix="navbar-nav"
-          >
+        <SidebarMenu.Collapse bsPrefix="navbar-collapse" className="w-auto">
+          <SidebarMenu.Nav as="ul" bsPrefix="navbar-nav">
             {
               sidebarLinks.map((item) => {
-                <a/>
+                <li/>
               })
             }
             <SidebarItemCollapsable id="profile-nav" title="Brooklyn Alice" className="mb-2" />
