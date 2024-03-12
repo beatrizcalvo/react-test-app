63512f0c5b89d79669caa87435468e361d9e0a33
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-12 16:26:44 +0100

    Update SidebarDashboard.js

--- src/components/sidebars/SidebarDashboard.js
+++ src/components/sidebars/SidebarDashboard.js
@@ -1,53 +1,9 @@
-import { Navbar, NavbarBrand, Nav } from "react-bootstrap";
+import { SidebarMenu } from 'react-bootstrap-sidebar-menu';
 
 export default function SidebarDashboard(props) {
   return (
     <>
-      <Navbar
-        as="aside"
-        expand="xs"
-        variant="vertical"
-        className="sidenav border-0 border-radius-xl my-3 fixed-start ms-3 bg-gradient-dark"
-      >
-        <div class="sidenav-header d-flex align-items-center justify-content-center">
-          <NavbarBrand 
-            as="a" 
-            className="m-0"
-          >
-            <img src={require("../../assets/img/logo-ct.png")} className="navbar-brand-img h-100" />
-            <span className="ms-1 font-weight-bold text-white">
-              React Test App
-            </span>
-          </NavbarBrand>
-        </div>
-        <hr class="horizontal light mt-0 mb-2" />
-        <Navbar.Collapse 
-          id="sidebar-dashboard-collapse" 
-          className="w-auto"
-        >
-          <Nav as="ul">
-            <Nav.Item 
-              as="li"
-              className="mb-2 mt-0"
-            >
-              <Navbar.Toggle 
-                as="a"
-                aria-controls="profile-nav" 
-                bsPrefix="nav-link" 
-                className="text-white"
-              >
-                <span className="nav-link-text ms-2 ps-1">
-                  Brooklyn Alice
-                </span>
-              </Navbar.Toggle>
-              <Navbar.Collapse id="profile-nav">
-                <Nav as="ul"></Nav>
-              </Navbar.Collapse>
-            </Nav.Item>
-            <hr class="horizontal light mt-0" />
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+      
     </>
   );
 };
