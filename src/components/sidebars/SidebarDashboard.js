dca441aa3688bb58cb47ed3cf2c200e70ea947f7
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-12 12:57:38 +0100

    Update SidebarDashboard.js

--- src/components/sidebars/SidebarDashboard.js
+++ src/components/sidebars/SidebarDashboard.js
@@ -9,8 +9,7 @@ export default function SidebarDashboard(props) {
         variant="vertical"
         className="sidenav border-0 border-radius-xl my-3 fixed-start ms-3 bg-gradient-dark"
       >
-        <div class="sidenav-header">
-          <i class="fas fa-times p-3 cursor-pointer text-white opacity-5 position-absolute end-0 top-0 d-none d-xl-none" />
+        <div class="sidenav-header d-flex align-items-center">
           <NavbarBrand 
             as="a" 
             className="m-0"
