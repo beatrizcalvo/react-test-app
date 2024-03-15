60f8596903f294c0f8f77a1408b64f61cfb0920b
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-15 11:16:06 +0100

    Update UserService.js

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+import { authHeader } from "AuthService";
+
 const loggedUser = () => {
-  
+  return axios.get(process.env.REACT_APP_AUTH_API + "/users/me", { headers: authHeader() });
 };
 
 const UserService = { 
