84140903569f007c2ef1be8a9d6fe9a8253cef9f
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-15 12:32:26 +0100

    Update SidebarDashboard.js

--- src/components/sidebars/SidebarDashboard.js
+++ src/components/sidebars/SidebarDashboard.js
@@ -65,7 +65,7 @@ export default function SidebarDashboard(props) {
   const sidebarLinks = [
     {
       id: "profile-nav",
-      title: "aaa",
+      title: props.userData.fullName,
       className: "mb-2",
       icon: null,
       links: [ 
