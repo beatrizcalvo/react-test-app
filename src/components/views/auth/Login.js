f58385cd83d0c3c980fcfccf6de8ac18e43747a9
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-17 00:38:48 +0100

    Update Login.js

--- src/components/views/auth/Login.js
+++ src/components/views/auth/Login.js
@@ -19,7 +19,6 @@ export default function Login(props) {
       .then((response) => {
         if (response.data) {
           // If login success then redirect to dashboard home page
-          alert(JSON.stringify(response.data));
           navigate("/");
           window.location.reload();
         } else {
