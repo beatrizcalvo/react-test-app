fa0eec9637a67d6e2afc185f24c2ab635e8fc12f
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-16 21:17:11 +0100

    Update AuthProvider.js

--- src/utils/AuthProvider.js
+++ src/utils/AuthProvider.js
@@ -1,8 +1,12 @@
 import { createContext, useContext } from 'react';
 
+import useLocalStorage from './LocalStorage';
+
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
+  const [token, setToken] = useLocalStorage("auth_token");
+  
   const contextValue = {};
   
   return <AuthContext.Provider value={contextValue}>{children}</AuthContext.Provider>;
