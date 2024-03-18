6b13dcb7fe869d92f14191a5fc95a1884e1788b0
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-18 13:33:15 +0100

    Update AuthProvider.js

--- src/utils/AuthProvider.js
+++ src/utils/AuthProvider.js
@@ -15,7 +15,7 @@ export const AuthProvider = ({ children }) => {
       password
     }).then(response => {
       setToken(response.data);
-      return response;
+      return getUserData();
     });
   };
 
