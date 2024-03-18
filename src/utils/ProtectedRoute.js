63ade7a3845a9e907f0abfafd6f7cabcf2a964f1
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-18 21:00:27 +0100

    Update ProtectedRoute.js

--- src/utils/ProtectedRoute.js
+++ src/utils/ProtectedRoute.js
@@ -7,7 +7,6 @@ export default function ProtectedRoute({
   children,
 }) {
   const { user } = useAuth();
-  console.log(JSON.stringify(user));
   
   if (!user) {
     return <Navigate to={redirectTo} />;
