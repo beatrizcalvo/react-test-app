e0eae3e631440d5482336867c16fdc5a7b7855a9
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-19 09:44:22 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,33 +1,10 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 
-import { AuthProvider, useAuth } from "./utils/AuthProvider";
 import ProtectedRoute from "./hooks/ProtectedRoute";
 
-import Dashboard from "./components/layouts/Dashboard";
-import Auth from "./components/layouts/Auth";
-import Home from "./components/views/Home";
-import Profile from "./components/views/Profile";
-import Login from "./components/views/auth/Login";
-import Register from "./components/views/auth/Register";
-
 export default function App(props) {
-  const { user } = useAuth();
   
   return (
-    <AuthProvider>
-      <Routes>
-        <Route element={<ProtectedRoute isAllowed={!!user} />}>
-          <Route element={<Dashboard />}>
-            <Route path="/" element={<Home />} />
-            <Route path="/profile" element={<Profile />} />
-          </Route>
-        </Route>
-        <Route element={<Auth />}>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-        </Route>
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
-    </AuthProvider>
+    
   );
 }
