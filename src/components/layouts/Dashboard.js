95c5a50dd729dd8644947fc0a594e731565b6f62
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-19 11:22:38 +0100

    Update Dashboard.js

--- src/components/layouts/Dashboard.js
+++ src/components/layouts/Dashboard.js
@@ -4,11 +4,17 @@ import { Container } from "react-bootstrap";
 
 import PerfectScrollbar from "perfect-scrollbar";
 
+import NavbarDashboard from "../navbars/NavbarDashboard";
+
 var ps;
 
 export default function Dashboard(props) {
   const mainPanelRef = useRef();
+  const navbarRef = useRef();
   const location = useLocation();
+
+  const navbarShowBlur = () => navbarRef.current.showBlur(true);
+  const navbarHideBlur = () => navbarRef.current.showBlur(false);
 
   useEffect(() => {
     document.body.classList.add("g-sidenav-show", "bg-gray-200");
@@ -16,11 +22,15 @@ export default function Dashboard(props) {
     if (navigator.platform.indexOf("Win") > -1) {
       ps = new PerfectScrollbar(mainPanelRef.current);
       document.body.classList.toggle("perfect-scrollbar-on");
+      mainPanelRef.current.addEventListener("ps-scroll-y", navbarShowBlur);
+      mainPanelRef.current.addEventListener("ps-y-reach-start", navbarHideBlur);
     }    
     return function cleanup() {
       if (navigator.platform.indexOf("Win") > -1) {
         ps.destroy();
         document.body.classList.toggle("perfect-scrollbar-on");
+        mainPanelRef.current.removeEventListener("ps-scroll-y", navbarShowBlur);
+        mainPanelRef.current.removeEventListener("ps-y-reach-start", navbarHideBlur);
       }
     };
   }, []);
@@ -33,6 +43,7 @@ export default function Dashboard(props) {
   return (
     <>
       <main className="main-content position-relative max-height-vh-100 h-100" ref={mainPanelRef}>
+        <NavbarDashboard ref={navbarRef} />
         <Container fluid className="py-4">
           <Outlet />
         </Container>
