d68b31305afa57cce98fbbde50343b34c24b9806
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-19 10:31:43 +0100

    Update AuthProvider.js

--- src/hooks/AuthProvider.js
+++ src/hooks/AuthProvider.js
@@ -1,4 +1,5 @@
 import { createContext, useContext, useMemo, useState } from 'react';
+import { useHistory } from "react-router-dom";
 
 import useLocalStorage from './LocalStorage';
 
@@ -6,10 +7,17 @@ const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(undefined);
+  const history = useHistory();
+
+  function login(email, password) {
+    setUser("Bbbbb");
+    history.push("/");
+  }
 
   // Make the provider update only when it should
   const memoedValue = useMemo(() => ({
-    user
+    user,
+    login
   }), [user]);
 
   return <AuthContext.Provider value={memoedValue}>{children}</AuthContext.Provider>;
