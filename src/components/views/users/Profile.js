bcc13536cce9e6aa0b20db0fb10f5920db5d4fcd
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-19 18:05:46 +0100

    Update Profile.js

--- src/components/views/users/Profile.js
+++ src/components/views/users/Profile.js
@@ -6,6 +6,8 @@ export default function Profile(props) {
   const { user } = useAuth();
 
   return (
+    <Container fluid className="px-2 px-md-4">
+    </Container>
     <div>
       <p>Profile Page</p>
       <p>Hello {JSON.stringify(user)}</p>
