e13959f4b1386cbf88579ad505b2422cee9d10f0
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-19 14:00:21 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import ProtectedRoute from "./hooks/ProtectedRoute";
 
 import Auth from "./components/layouts/Auth";
 import Login from "./components/views/auth/Login";
+import Register from "./components/views/auth/Register";
 import Dashboard from "./components/layouts/Dashboard";
 import Home from "./components/views/Home";
 import Profile from "./components/views/Profile";
@@ -22,6 +23,7 @@ export default function App(props) {
       </Route>
       <Route element={<Auth />}>
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Route>
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
