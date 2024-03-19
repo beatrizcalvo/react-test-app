4dfb2f4f87b81a0e1a596cfa7c134f86b40c8554
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-19 10:46:35 +0100

    Update AuthProvider.js

--- src/hooks/AuthProvider.js
+++ src/hooks/AuthProvider.js
@@ -1,5 +1,5 @@
 import { createContext, useContext, useMemo, useState } from 'react';
-import { useHistory } from "react-router-dom";
+import useHistory from "react-router-dom";
 
 import useLocalStorage from './LocalStorage';
 
