9183302a61189190130ceea339af54cd0a345157
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-19 09:58:24 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 
+import { AuthProvider, useAuth } from "./hooks/AuthProvider";
 import ProtectedRoute from "./hooks/ProtectedRoute";
 
 export default function App(props) {
+  const { user } = useAuth();
   
   return (
-    
+    <AuthProvider>
+      <Routes>
+      </Routes>
+    </AuthProvider>
   );
 }
