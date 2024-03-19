4ecc6db10c662611e530682cced53f89c44d747f
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-20 00:56:01 +0100

    Update CardProfile.js

--- src/components/cards/users/CardProfile.js
+++ src/components/cards/users/CardProfile.js
@@ -1,7 +1,12 @@
 import { Card } from "react-bootstrap";
 
 export default function CardProfile(props) {
+  const { id, profile } = props;
+  
   return (
-    
+    <>
+      <Card.Body id={id}>
+      </Card.Body>
+    </>
   );
 }
