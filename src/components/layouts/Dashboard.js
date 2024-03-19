c49b79ad8d721c46ff2f4cce2b9740866a94325d
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-19 12:26:18 +0100

    Update Dashboard.js

--- src/components/layouts/Dashboard.js
+++ src/components/layouts/Dashboard.js
@@ -18,14 +18,15 @@ export default function Dashboard(props) {
 
   useEffect(() => {
     document.body.classList.add("g-sidenav-show", "bg-gray-200");
-
     if (navigator.platform.indexOf("Win") > -1) {
       ps = new PerfectScrollbar(mainPanelRef.current);
       document.body.classList.toggle("perfect-scrollbar-on");
       mainPanelRef.current.addEventListener("ps-scroll-y", navbarShowBlur);
       mainPanelRef.current.addEventListener("ps-y-reach-start", navbarHideBlur);
     }    
+    
     return function cleanup() {
+      document.body.classList.remove("g-sidenav-show", "bg-gray-200");
       if (navigator.platform.indexOf("Win") > -1) {
         ps.destroy();
         document.body.classList.toggle("perfect-scrollbar-on");
