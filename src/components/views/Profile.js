c1afd0025ad26ea4fa7617f80b667d8ca7a32ed2
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-19 09:28:44 +0100

    Update Profile.js

--- src/components/views/Profile.js
+++ src/components/views/Profile.js
@@ -1,9 +1,14 @@
 import { Container } from "react-bootstrap";
 
+import { useAuth } from "./AuthProvider";
+
 export default function Profile(props) {
+  const { user } = useAuth();
+  
   return (
     <>
       <Container>
+        { JSON.stringify(user) }
       </Container>
     </>
   );
