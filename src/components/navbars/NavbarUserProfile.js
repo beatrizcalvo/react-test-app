50364bf0553849496e5ad53c61d1a487fada04b8
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-19 23:18:41 +0100

    Update NavbarUserProfile.js

--- src/components/navbars/NavbarUserProfile.js
+++ src/components/navbars/NavbarUserProfile.js
@@ -2,19 +2,22 @@ import { Nav } from "react-bootstrap";
 
 export default function NavbarUserProfile(props) {
 
-  const navbarLinks = [];
+  const navbarLinks = [
+    {}
+  ];
   
   return (
     <>
       <Nav as="ul" className="flex-column bg-white border-radius-lg p-3">
-      {
-        navbarLinks.map((item, index) => {
-          return (
-            <Nav.Item as="li">
-              <Nav.Link></Nav.Link>
-            </Nav.Item>
-          );
-        })
+       {
+          navbarLinks.map((item, index) => {
+            return (
+              <Nav.Item as="li">
+                <Nav.Link></Nav.Link>
+              </Nav.Item>
+            );
+          })
+        }
       </Nav>
     </>
   );
