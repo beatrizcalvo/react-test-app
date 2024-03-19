3881ce2ff34114a0fcf793784be513d24ba7f3ac
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-19 23:50:14 +0100

    Update Profile.js

--- src/components/views/users/Profile.js
+++ src/components/views/users/Profile.js
@@ -6,12 +6,20 @@ import NavbarUserProfile from "../../navbars/NavbarUserProfile";
 export default function Profile(props) {
   const { user } = useAuth();
 
+  const navbarLinks = [
+    {
+      href: "#profile",
+      title: "Profile",
+      icon: "fa-solid fa-user"
+    }
+  ];
+
   return (
     <>
       <Row className="mb-5">
         <Col lg="3">
           <Card className="position-sticky top-1">
-            <NavbarUserProfile />
+            <NavbarUserProfile navbarLinks={navbarLinks} />
           </Card>
         </Col>
       </Row>
