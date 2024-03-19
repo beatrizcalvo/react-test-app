b410c9c472bf8f8df04ff55a648649ad50cad75a
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-19 11:55:10 +0100

    Update Dashboard.js

--- src/components/layouts/Dashboard.js
+++ src/components/layouts/Dashboard.js
@@ -29,8 +29,8 @@ export default function Dashboard(props) {
       if (navigator.platform.indexOf("Win") > -1) {
         ps.destroy();
         document.body.classList.toggle("perfect-scrollbar-on");
-        mainPanelRef.current.removeEventListener("ps-scroll-y", navbarShowBlur);
-        mainPanelRef.current.removeEventListener("ps-y-reach-start", navbarHideBlur);
+        mainPanelRef.current.removeEventListener("ps-scroll-y");
+        mainPanelRef.current.removeEventListener("ps-y-reach-start");
       }
     };
   }, []);
