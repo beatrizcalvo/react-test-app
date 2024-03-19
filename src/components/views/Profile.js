30b20e71737ce3f7e6198d7c8c2c0efbc4e84a03
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-19 10:35:48 +0100

    Create Profile.js

---
+++ src/components/views/Profile.js
@@ -0,0 +1,14 @@
+import { Container } from "react-bootstrap";
+
+import { useAuth } from "../../../hooks/AuthProvider";
+
+export default function Profile(props) {
+  const { user } = useAuth();
+
+  return (
+    <div>
+      <p>Profile Page</p>
+      <p>Hello {user}</p>
+    </div>
+  );
+}
