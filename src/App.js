1524f810367bb3f9b46e583741130e15dcf8031c
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-19 11:00:07 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 
-import { AuthProvider, useAuth } from "./hooks/AuthProvider";
+import { useAuth } from "./hooks/AuthProvider";
 import ProtectedRoute from "./hooks/ProtectedRoute";
 
 import Auth from "./components/layouts/Auth";
 import Login from "./components/views/auth/Login";
 import Dashboard from "./components/layouts/Dashboard";
+import Home from "./components/views/Home";
+import Profile from "./components/views/Profile";
 
 export default function App(props) {
   const { user } = useAuth();
   
   return (
-    <AuthProvider>
-      <Routes>
-        <Route element={<ProtectedRoute isAllowed={!!user} />}>
-          <Route path="/" element={<Dashboard />} />
+    <Routes>
+      <Route element={<ProtectedRoute isAllowed={!!user} />}>
+        <Route element={<Dashboard />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
         </Route>
-        <Route element={<Auth />}>
-          <Route path="/login" element={<Login />} />
-        </Route>
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
-    </AuthProvider>
+      </Route>
+      <Route element={<Auth />}>
+        <Route path="/login" element={<Login />} />
+      </Route>
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
   );
 }
