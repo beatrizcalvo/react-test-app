3cd4593d1a72b1ec53dc8519f186367b20989a41
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-19 23:04:54 +0100

    Update Profile.js

--- src/components/views/users/Profile.js
+++ src/components/views/users/Profile.js
@@ -1,6 +1,7 @@
-import { Container, Row, Col, Card, Nav } from "react-bootstrap";
+import { Container, Row, Col, Card } from "react-bootstrap";
 
 import { useAuth } from "../../../hooks/AuthProvider";
+import NavbarUserProfile from "../../navbars/NavbarUserProfile";
 
 export default function Profile(props) {
   const { user } = useAuth();
@@ -10,9 +11,7 @@ export default function Profile(props) {
       <Row className="mb-5">
         <Col lg="3">
           <Card className="position-sticky top-1">
-            <Nav as="ul" className="flex-column bg-white border-radius-lg p-3">
-              <Nav.Item></Nav.Item>
-            </Nav>
+            <NavbarUserProfile />
           </Card>
         </Col>
       </Row>
