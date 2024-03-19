430efbfdac8f9aea4c47a851c405ede61a529416
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-19 12:08:22 +0100

    Update Login.js

--- src/components/views/auth/Login.js
+++ src/components/views/auth/Login.js
@@ -1,30 +1,35 @@
 import { useAuth } from "../../../hooks/AuthProvider";
+import LoginForm from "../../forms/LoginForm";
 
 export default function Login(props) {
   const { loginUser } = useAuth();
   
-  function handleSubmit(event) {
-    event.preventDefault();
+  function handleLogin() {
     loginUser("aaa", "bbbb");
   };
   
   return (
-    <form onSubmit={handleSubmit}>
-      <h1>Login</h1>
-
-      <label htmlFor="email">
-        Email
-        <input id="email" name="email" />
-      </label>
-
-      <label htmlFor="password">
-        Password
-        <input id="password" name="password" type="password" />
-      </label>
-
-      <button type="submit">
-        Submit
-      </button>
-    </form>
+    <>
+      <div className="card card-plain">
+        <div className="card-header text-center">
+          <h4 className="font-weight-bolder">Sign In</h4>
+          <p className="mb-0">Enter your email and password to sign in</p>
+        </div>
+        <div className="card-body mt-2">
+          <LoginForm handleLogin={handleLogin} />
+        </div>
+        <div className="card-footer text-center pt-0 px-lg-2 px-1">
+          <p className="mb-4 text-sm mx-auto">
+            Don't have an account?{" "}
+            <Link
+              to="/register"
+              className="text-primary text-gradient font-weight-bold"
+            >
+              Sign up
+            </Link>
+          </p>
+        </div>
+      </div>
+    </>
   );
 }
