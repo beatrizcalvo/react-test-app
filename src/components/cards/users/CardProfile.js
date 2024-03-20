eba35ee29cfd56b94d1e5d42bce64bbeb18de2c8
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-20 01:59:26 +0100

    Update CardProfile.js

--- src/components/cards/users/CardProfile.js
+++ src/components/cards/users/CardProfile.js
@@ -5,7 +5,7 @@ export default function CardProfile(props) {
   
   return (
     <>
-      <Card.Body id={id} className="card text-start">
+      <Card.Body id={id} className="card">
         <Row className="justify-content-center align-items-center">
           <Col sm="auto" className="col-4">
             <div className="avatar avatar-xl position-relative">
