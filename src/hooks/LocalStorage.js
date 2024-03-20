1883b9f4bcd1e5b7d9afac001bbe4c7720c8943e
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-20 10:21:03 +0100

    Update LocalStorage.js

--- src/hooks/LocalStorage.js
+++ src/hooks/LocalStorage.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 export default function useLocalStorage(key) {
   const [value, setValue] = useState(() => {
       try {
+        console.log("Recupera token");
         const storedValue = localStorage.getItem(key);
         return storedValue ? JSON.parse(storedValue) : undefined;
       } catch (error) {
@@ -12,6 +13,8 @@ export default function useLocalStorage(key) {
   
     useEffect(() => {
       try {
+        console.log("update token");
+        console.log("JSON.stringify(value));
         localStorage.setItem(key, JSON.stringify(value));
       } catch (error) {
         console.error(error);
