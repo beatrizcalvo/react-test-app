da0d3b997a0b962150fcf3493f9dc7f2fb2a622c
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-20 01:51:47 +0100

    Update CardProfile.js

--- src/components/cards/users/CardProfile.js
+++ src/components/cards/users/CardProfile.js
@@ -9,7 +9,7 @@ export default function CardProfile(props) {
         <Row className="justify-content-center align-items-center">
           <Col sm="auto" className="col-4">
             <div className="avatar avatar-xl position-relative">
-              <img src={require("../../../assets/img/logo-ct.png")} className="w-100 rounded-circle shadow-sm" />
+              <img src={require("../../../assets/img/default_profile.jpg")} className="w-100 rounded-circle shadow-sm" />
               <span className="position-absolute top-100 start-100 translate-middle bg-primary border border-light rounded-circle">
                 <i className="fa-solid fa-camera"/>
               </span>
