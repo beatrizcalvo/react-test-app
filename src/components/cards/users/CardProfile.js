4e64cb499aaeda90723df7487fa6a0f1de5986b4
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-20 02:23:15 +0100

    Update CardProfile.js

--- src/components/cards/users/CardProfile.js
+++ src/components/cards/users/CardProfile.js
@@ -10,14 +10,16 @@ export default function CardProfile(props) {
           <Col sm="auto" className="col-4">
             <div className="avatar avatar-xl position-relative">
               <img src={require("../../../assets/img/default_profile.jpg")} className="w-100 rounded-circle shadow-sm" />
-              <span className="position-absolute top-100 start-100 translate-middle bg-primary p-2 border border-light rounded-circle fs-6">
+              <span className="position-absolute top-100 start-100 translate-middle bg-primary border border-light rounded-circle fs-6">
                 <i className="fa-solid fa-camera"/>
               </span>
             </div>
           </Col>
           <Col sm="auto" className="col-8 my-auto">
             <div className="h-100">
-    
+              <h5 class="mb-1 font-weight-bolder">
+                { profile.fullName }
+              </h5>
             </div>
           </Col>
         </Row>
