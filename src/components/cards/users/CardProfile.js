c36290442c48fa115f6c9c8ffa3f951bb72f58d7
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-20 01:36:25 +0100

    Update CardProfile.js

--- src/components/cards/users/CardProfile.js
+++ src/components/cards/users/CardProfile.js
@@ -1,4 +1,4 @@
-import { Card } from "react-bootstrap";
+import { Card, Row, Col } from "react-bootstrap";
 
 export default function CardProfile(props) {
   const { id, profile } = props;
@@ -6,6 +6,13 @@ export default function CardProfile(props) {
   return (
     <>
       <Card.Body id={id}>
+        <Row className="justify-content-center align-items-center">
+          <Col sm="auto" className="col-4">
+            <div className="avatar avatar-xl position-relative">
+              <img src={require("../../assets/img/logo-ct.png")} className="w-100 rounded-circle shadow-sm" />
+            </div>
+          </Col>
+        </Row>
       </Card.Body>
     </>
   );
