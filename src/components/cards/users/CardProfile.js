ce9f9c3d0224311da9f9cec0b112aa7e5ce5a753
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-20 10:36:18 +0100

    Update CardProfile.js

--- src/components/cards/users/CardProfile.js
+++ src/components/cards/users/CardProfile.js
@@ -10,7 +10,7 @@ export default function CardProfile(props) {
           <Col sm="auto" className="col-4">
             <div className="avatar avatar-xl position-relative">
               <img src={require("../../../assets/img/default_profile.jpg")} className="w-100 border-radius-lg shadow-sm" />
-              <span className="position-absolute top-100 start-100 translate-middle badge badge-circle bg-info">
+              <span className="position-absolute top-100 start-100 translate-middle badge badge-circle bg-gradient-primary">
                 <i className="fa-solid fa-camera" />
               </span>
             </div>
