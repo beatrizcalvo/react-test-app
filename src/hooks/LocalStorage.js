085dbc2532ea2f7420aecc85a8966173ed7c537e
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-20 12:37:46 +0100

    Update LocalStorage.js

--- src/hooks/LocalStorage.js
+++ src/hooks/LocalStorage.js
@@ -1,13 +1,20 @@
-import { useEffect, useState } from "react";
-
-export default function useLocalStorage(storageKey) {
-  const [storedValue, setStoredValue] = useState(
-    JSON.parse(localStorage.getItem(storageKey)) ?? undefined
-  );
-
-  useEffect(() => {
-    localStorage.setItem(storageKey, JSON.stringify(storedValue));
-  }, [storedValue, storageKey]);
+export default function useLocalStorage(key) {
+  const [value, setValue] = useState(() => {
+      try {
+        const storedValue = localStorage.getItem(key);
+        return storedValue ? JSON.parse(storedValue) : undefined;
+      } catch (error) {
+        return undefined;
+      }
+    });
   
-  return [storedValue, setStoredValue];
+    useEffect(() => {
+      try {
+        localStorage.setItem(key, JSON.stringify(value));
+      } catch (error) {
+        console.error(error);
+      }
+    }, [key, value]);
+  
+    return [value, setValue];
 };
