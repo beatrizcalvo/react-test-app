18f936c5aa39647e73be0689b79bbf2ff97e0ad8
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-21 12:19:56 +0100

    Update CardProfile.js

--- src/components/cards/users/CardProfile.js
+++ src/components/cards/users/CardProfile.js
@@ -1,4 +1,4 @@
-import { Card, Row, Col } from "react-bootstrap";
+import { Card, Row, Col, Button } from "react-bootstrap";
 
 export default function CardProfile(props) {
   const { id, profile } = props;
@@ -10,9 +10,11 @@ export default function CardProfile(props) {
           <Col sm="auto" className="col-4">
             <div className="avatar avatar-xl position-relative">
               <img src={require("../../../assets/img/default_profile.jpg")} className="w-100 border-radius-lg shadow-sm" />
-              <span className="position-absolute top-100 start-100 translate-middle badge badge-circle bg-gradient-primary">
+              <Button
+                className="position-absolute top-100 start-100 translate-middle badge badge-circle bg-gradient-primary"
+              >                
                 <i className="fa-solid fa-camera" />
-              </span>
+              </Button>
             </div>
           </Col>
           <Col sm="auto" className="col-8 my-auto">
