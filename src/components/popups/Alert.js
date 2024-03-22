ca5ee87b4a0847f685dd8a5771c4a4f22f8bcf1c
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-22 08:34:06 +0100

    Update Alert.js

--- src/components/popups/Alert.js
+++ src/components/popups/Alert.js
@@ -1,14 +1,21 @@
+import { useState } from "react";
 import { ToastContainer, Toast } from "react-bootstrap";
 
 export default function Alert(props) {
   const { show, variant } = props;
+  const [showAlert, setShowAlert] = useState(show);
   
   return (
     <>
       <ToastContainer position="top-end">
-        <Toast bg={variant} show={show} delay={3000} autohide>
+        <Toast bg={variant} show={showAlert} delay={3000} autohide onClose={() => setShowAlert(false)}>
           <Toast.Body>
-            Prueba Alert
+            <div className="d-flex gap-4">
+              <span><i className="fa-solid fa-circle-check fa-lg" /></span>
+              <div className="d-flex flex-grow-1 aling-items-center">
+                Prueba Alert
+              </div>
+            </div>
           </Toast.Body>
         </Toast>
       </ToastContainer>
