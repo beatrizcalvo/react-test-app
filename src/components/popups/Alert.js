dde6c4e872b4f707d0d5d141c5b902bcf90e060c
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-22 08:18:52 +0100

    Update Alert.js

--- src/components/popups/Alert.js
+++ src/components/popups/Alert.js
@@ -1,12 +1,12 @@
 import { ToastContainer, Toast } from "react-bootstrap";
 
 export default function Alert(props) {
-  const { variant } = props;
+  const { show, variant } = props;
   
   return (
     <>
       <ToastContainer position="top-end">
-        <Toast bg={variant} delay={3000} autohide>
+        <Toast bg={variant} show={show} delay={3000} autohide>
           <Toast.Body>
             Prueba Alert
           </Toast.Body>
