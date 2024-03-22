811af26e29524fd681294ffb8a5462d811f959ef
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-22 10:40:25 +0100

    Update Alert.js

--- src/components/popups/Alert.js
+++ src/components/popups/Alert.js
@@ -26,7 +26,7 @@ export default function Alert(props) {
             <span><i className={alertIcon() + " fa-lg"} /></span>
             <div className="d-flex flex-grow-1 aling-items-center">
               <span>
-                <strong><em>{ (variant === "danger") ? "Error!!" : "Success!!" }</em></strong>
+                <strong><em>{ (variant === "danger") ? "ERROR!!" : "SUCCESS!!" }</em></strong>
                 <p>Hello, world! This is a toast message.</p>
               </span>
               <Button 
