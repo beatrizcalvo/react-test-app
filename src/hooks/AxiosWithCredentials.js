c74ccfa94b632901e6c1ba4c7c5058d241e68883
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-22 15:01:17 +0100

    Update AxiosWithCredentials.js

--- src/hooks/AxiosWithCredentials.js
+++ src/hooks/AxiosWithCredentials.js
@@ -5,6 +5,16 @@ const AUTH_TOKEN_KEY = "access_token";
 
 export default function axiosWithCredentials (baseURL) {
   const axiosInstance = axios.create({ baseURL: baseURL });
+
+  axiosInstance.interceptors.request.use(
+    async (config) => config,
+    (error) => Promise.reject(error)
+  );
+
+  axiosInstance.interceptors.response.use(
+    (response) => response,
+    async (error) => Promise.reject(error)
+  );
   
-  return axiosInstance(baseURL);
+  return axiosInstance;
 };
