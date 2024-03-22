3f29bc471bf2c1ff6f7ff76b9ced471dace37fb9
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-22 08:03:49 +0100

    Update Alert.js

--- src/components/popups/Alert.js
+++ src/components/popups/Alert.js
@@ -1,5 +1,15 @@
 import { ToastContainer, Toast } from "react-bootstrap";
 
 export default function Alert(props) {
-  
+  return (
+    <>
+      <ToastContainer position="top-end">
+        <Toast>
+          <Toast.Body>
+            Prueba Alert
+          </Toast.Body>
+        </Toast>
+      </ToastContainer>
+    </>
+  );
 }
