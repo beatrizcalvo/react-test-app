c8e0ba216ce6f51f37c64fd0a8eff007952a122c
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-22 14:38:52 +0100

    Create UsersService.js

---
+++ src/services/UsersService.js
@@ -0,0 +1,12 @@
+import  secureLocalStorage  from  "react-secure-storage";
+import useAxiosWithCredentials from "../hooks/AxiosWithCredentials";
+
+const axiosCredentials = useAxiosWithCredentials(process.env.REACT_APP_AUTH_API); 
+
+const getCurrentUser = () => return axiosCredentials.get("/users/me");
+
+const UsersService = { 
+  getCurrentUser
+};
+
+export default UsersService;
