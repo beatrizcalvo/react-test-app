8a5e22f7a8181718fe71d1935fd3fc2221a4161c
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-22 08:51:55 +0100

    Update Alert.js

--- src/components/popups/Alert.js
+++ src/components/popups/Alert.js
@@ -8,10 +8,10 @@ export default function Alert(props) {
       <ToastContainer position="top-end">
         <Toast bg={variant} show={show} delay={3000} autohide onClose={() => setShow(false)}>
           <Toast.Body>
-            <div className="d-flex gap-4">
+            <div className="d-flex gap-4 text-white">
               <span><i className="fa-solid fa-circle-check fa-lg" /></span>
               <div className="d-flex flex-grow-1 aling-items-center">
-                Prueba Alert
+                <span className="fw-semibold">Prueba Alert</span>
               </div>
             </div>
           </Toast.Body>
