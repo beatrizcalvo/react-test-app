5f315f8938755e5da8efa138c4e115ac3d290221
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-22 09:44:54 +0100

    Update Alert.js

--- src/components/popups/Alert.js
+++ src/components/popups/Alert.js
@@ -17,18 +17,13 @@ export default function Alert(props) {
   return (
     <>
       <ToastContainer position="top-end">
-        <Toast bg={variant} show={show} delay={5000} autohide onClose={() => setShow(false)}>
+        <Toast>
           <Toast.Header>
-            <img src="holder.js/20x20?text=%20" className="rounded me-2" />
+            <span><i className={alertIcon() + " fa-lg"} /></span>
+            <strong className="me-auto">Bootstrap</strong>
+            <small>11 mins ago</small>
           </Toast.Header>
-          <Toast.Body>
-            <div className="d-flex gap-4 text-white">
-              <span><i className={alertIcon() + " fa-lg"} /></span>
-              <div className="d-flex flex-grow-1 aling-items-center">
-                <span className="fw-semibold">Prueba Alert</span>
-              </div>
-            </div>
-          </Toast.Body>
+          <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
         </Toast>
       </ToastContainer>
     </>
