879989ef6b3eb614846c572198fa845efc210997
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-22 13:31:32 +0100

    Update AxiosWithCredentials.js

--- src/hooks/AxiosWithCredentials.js
+++ src/hooks/AxiosWithCredentials.js
@@ -1,10 +1,20 @@
 import axios from "axios";
 import  secureLocalStorage  from  "react-secure-storage";
 
+const AUTH_TOKEN_KEY = "access_token";
 const axiosInstance = Axios.create({ baseURL: baseURL });
 
 axiosInstance.interceptors.request.use(
-  async (config) => { return config },
+  async (config) => { 
+    const token = secureLocalStorage.getItem(AUTH_TOKEN_KEY);
+    if (token) {
+      config.headers = {
+        ...config.headers,
+        authorization: `Bearer ${token}`
+      };
+    }
+    return config 
+  },
   (error) => { Promise.reject(error) }
 );
 
