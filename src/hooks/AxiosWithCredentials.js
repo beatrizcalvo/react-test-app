55c20362ee44437295b79e08616e2fff759f8132
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-22 14:52:56 +0100

    Update AxiosWithCredentials.js

--- src/hooks/AxiosWithCredentials.js
+++ src/hooks/AxiosWithCredentials.js
@@ -2,8 +2,9 @@ import axios from "axios";
 import  secureLocalStorage  from  "react-secure-storage";
 
 const AUTH_TOKEN_KEY = "access_token";
-const axiosInstance = (baseURL) => { return axios.create({ baseURL: baseURL }) };
 
-export default function useAxiosWithCrendetials (baseURL) {
+export default function axiosWithCredentials (baseURL) {
+  const axiosInstance = axios.create({ baseURL: baseURL });
+  
   return axiosInstance(baseURL);
 };
