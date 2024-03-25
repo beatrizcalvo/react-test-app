e3be7033e8aca8cf07cfc0bc3803a7ce9e172ada
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-25 12:19:41 +0100

    Update Dashboard.js

--- src/components/layouts/Dashboard.js
+++ src/components/layouts/Dashboard.js
@@ -52,7 +52,7 @@ export default function Dashboard(props) {
       <SidebarDashboard />
       <main className="main-content position-relative max-height-vh-100 h-100" ref={mainPanelRef}>
         <NavbarDashboard ref={navbarRef} />
-        <Container fluid className="px-4">
+        <Container fluid className="px-4 py-4">
           <Outlet />
         </Container>
       </main>
