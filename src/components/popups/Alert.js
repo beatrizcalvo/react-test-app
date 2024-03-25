d33ce6263e189024badb1d3cb8c4e2bc6631e20b
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-25 11:59:01 +0100

    Update Alert.js

--- src/components/popups/Alert.js
+++ src/components/popups/Alert.js
@@ -1,45 +1,8 @@
-import { ToastContainer, Toast, Button } from "react-bootstrap";
-
 export default function Alert(props) {
-  const { show, setShow, variant, message } = props;
-
-  const alertIcon = () => {
-    switch(variant) {
-      case "danger":
-        return "fa-solid fa-circle-exclamation";
-      default:
-        return "fa-solid fa-circle-check";
-    }
-  };
   
   return (
     <>
-      <ToastContainer position="top-end">
-        <Toast 
-          bg={variant}
-          show={show}
-          autohide
-          delay={10000}
-          onClose={() => setShow(false)}
-        >
-          <Toast.Body className="d-flex gap-3 text-white">
-            <span><i className={alertIcon() + " fa-lg"} /></span>
-            <div className="d-flex flex-grow-1 aling-items-center">
-              <span>
-                <strong>{ (variant === "danger") ? "ERROR!!" : "SUCCESS!!" }</strong>
-                <p className="text-xs">{message}</p>
-              </span>
-              <Button 
-                bsPrefix="btn-close" 
-                className="text-white btn-close-sm ms-auto" 
-                onClick={() => setShow(false)}
-              >
-                <i className="fa-solid fa-xmark"/>
-              </Button>
-            </div>
-          </Toast.Body>
-        </Toast>
-      </ToastContainer>
+      
     </>
   );
 }
