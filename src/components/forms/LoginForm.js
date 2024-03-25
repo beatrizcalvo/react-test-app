7c8cc2f6cb26db8e03e7e89dfa9f192e0ce4978a
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-25 11:53:40 +0100

    Update LoginForm.js

--- src/components/forms/LoginForm.js
+++ src/components/forms/LoginForm.js
@@ -1,7 +1,7 @@
 import { Form, Button, Alert, Spinner } from "react-bootstrap";
 import { useForm } from "react-hook-form";
 
-import { useAuth } from "../../hooks/AuthProvider";
+import { useAuth } from "../../hooks/providers/AuthProvider";
 
 export default function LoginForm(props) {
   const connectionError = "Cannot connect to the user registration server.";
