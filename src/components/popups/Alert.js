01113bd07e37cab49001bce5745525a6ef6090c8
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-25 13:37:43 +0100

    Update Alert.js

--- src/components/popups/Alert.js
+++ src/components/popups/Alert.js
@@ -1,10 +1,32 @@
 import { Toast } from "react-bootstrap";
 
 export default function Alert(props) {
+  const { variant, message } = props;
+
+  // Get icon depending on variant value
+  const alertIcon = () => {
+    switch(variant) {
+      case "danger":
+        return "fa-solid fa-circle-exclamation";
+      default:
+        return "fa-solid fa-circle-check";
+    }
+  };
   
   return (
     <>
-      <Toast>
+      <Toast 
+        bg={variant}
+      >
+        <Toast.Body className="d-flex gap-3 text-white">
+          <span><i className={alertIcon() + " fa-lg"} /></span>
+          <div className="d-flex flex-grow-1 aling-items-center">
+            <span>
+              <strong>{ (variant === "danger") ? "ERROR!!" : "SUCCESS!!" }</strong>
+              <p className="text-xs">{message}</p>
+            </span>
+          </div>
+        </Toast.Body>
       </Toast>
     </>
   );
