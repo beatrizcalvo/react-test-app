5a664fe4e848405cf02da5126ea0418d6dfaffda
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-25 09:58:10 +0100

    Update Dashboard.js

--- src/components/layouts/Dashboard.js
+++ src/components/layouts/Dashboard.js
@@ -1,15 +1,18 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Outlet, useLocation } from "react-router-dom";
-import { Container } from "react-bootstrap";
+import { Container, ToastContainer } from "react-bootstrap";
 
 import PerfectScrollbar from "perfect-scrollbar";
 
 import SidebarDashboard from "../sidebars/SidebarDashboard";
 import NavbarDashboard from "../navbars/NavbarDashboard";
+import Alert from "../popups/Alert";
 
 var ps;
 
 export default function Dashboard(props) {
+  const [alerts, setAlerts] = useState([]);
+  
   const mainPanelRef = useRef();
   const navbarRef = useRef();
   const location = useLocation();
@@ -27,19 +30,25 @@ export default function Dashboard(props) {
     
     return () => {
       document.body.classList.remove("g-sidenav-show", "bg-gray-200");
-  
-      ps.destroy();
+
+      mainPanelRef.current.addEventListener("ps-scroll-y", navbarShowBlur);
+      mainPanelRef.current.addEventListener("ps-y-reach-start", navbarHideBlur);  
       document.body.classList.toggle("perfect-scrollbar-on");
+      ps.destroy();
     };
   }, []);
 
   useEffect(() => {
     mainPanelRef.current.scrollTop = 0;
     document.scrollingElement.scrollTop = 0;
+    if (alerts.length !== 0) setAlerts([]);
   }, [location]);
 
   return (
     <>
+      <ToastContainer position="bottom-end">
+        { alerts.map(item => { return <Alert {...item} />; }) }
+      </ToastContainer>
       <SidebarDashboard />
       <main className="main-content position-relative max-height-vh-100 h-100" ref={mainPanelRef}>
         <NavbarDashboard ref={navbarRef} />
