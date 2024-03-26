e345d18fb015e6a17c02cc9c6eb7b0127719681e
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-26 09:12:45 +0100

    Update AxiosWithCredentials.js

--- src/hooks/AxiosWithCredentials.js
+++ src/hooks/AxiosWithCredentials.js
@@ -32,51 +32,8 @@ export default function axiosWithCredentials (baseURL) {
   axiosInstance.interceptors.response.use(
     (response) => { return response },
     async (error) => {
-      const token = secureLocalStorage.getItem(REFRESH_TOKEN_KEY);
-      const originalRequest: AxiosRequestConfig = error.config;
-      if (!!token && error.response && error.response.status === 401) {
-        if (!isRefreshing) {
-          isRefreshing = true;
-          try {
-            // Refresh the access token
-            const refreshResponse = await axios.post(baseURL + "/auth/refresh", {
-              refresh_token: secureLocalStorage.getItem(REFRESH_TOKEN_KEY)
-            });
-
-            // Update the request headers and localstorage with the new access token
-            const newAccessToken = refreshResponse.access_token;
-            error.config.headers['Authorization'] = `Bearer ${newAccessToken}`;
-            secureLocalStorage.setItem(ACCESS_TOKEN_KEY, newAccessToken);
-
-            // Retry all requests in the queue with the new token
-            refreshAndRetryQueue.forEach(({ config, resolve, reject }) => {
-              axiosInstance
-                .request(config)
-                .then((response) => resolve(response))
-                .catch((err) => reject(err));
-            });
-
-            // Clear the queue
-            refreshAndRetryQueue.length = 0;
-
-            // Retry the original request
-            return axiosInstance(originalRequest);
-          } catch (refreshError) {
-            throw refreshError;
-          } finally {
-            isRefreshing = false;
-          }
-        }
-
-        // Add the original request to the queue
-        return new Promise((resolve, reject) => {
-          refreshAndRetryQueue.push({ config: originalRequest, resolve, reject });
-        });
-      }
-      
       // Return a Promise rejection if the status code is not 401
-      return Promise.reject(error);
-    }
+      return Promise.reject(error)
   );
   
   return axiosInstance;
