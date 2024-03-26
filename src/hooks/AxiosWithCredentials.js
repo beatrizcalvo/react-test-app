226583b0379234b075eec9d0e61fb1e630c89b21
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-26 12:12:22 +0100

    Update AxiosWithCredentials.js

--- src/hooks/AxiosWithCredentials.js
+++ src/hooks/AxiosWithCredentials.js
@@ -4,48 +4,6 @@ import secureLocalStorage from  "react-secure-storage";
 const ACCESS_TOKEN_KEY = "access_token";
 const REFRESH_TOKEN_KEY = "refresh_token";
 
-interface RetryQueueItem {
-  resolve: (value?: any) => void;
-  reject: (error?: any) => void;
-  config: AxiosRequestConfig;
-}
+const axiosInstance = axios.create({ baseURL: process.env.REACT_APP_AUTH_API });
 
-export default function axiosWithCredentials (baseURL) {
-  const axiosInstance = axios.create({ baseURL: baseURL });
-  const refreshAndRetryQueue: RetryQueueItem[] = [];
-  let isRefreshing = false;
-
-  axiosInstance.interceptors.request.use(
-    (config) => {
-      const accessToken = secureLocalStorage.getItem(ACCESS_TOKEN_KEY);
-      if (accessToken) {
-        config.headers = {
-          ...config.headers,
-          authorization: `Bearer ${accessToken}`
-        };
-      }
-      return config;
-    },
-    (error) => Promise.reject(error)
-  );
-
-  axiosInstance.interceptors.response.use(
-    (response) => response,
-    async (error) => {
-      const refreshToken = secureLocalStorage.getItem(REFRESH_TOKEN_KEY);
-      const originalRequest: AxiosRequestConfig = error.config;
-
-      try {
-        // Refresh the access token
-        const refreshResponse = await axios.post(baseURL + "/auth/refresh", { refresh_token: refreshToken });
-      } catch (error) {
-        console.log(error);
-      }
-      
-      // Return a Promise rejection if error occurs
-      return Promise.reject(error);
-    }
-  );
-  
-  return axiosInstance;
-};
+export default axiosInstance;
