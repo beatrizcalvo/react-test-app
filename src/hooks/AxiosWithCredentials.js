ab27567c16699d04e38a90d1637e2a2d547883a2
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-26 12:12:49 +0100

    Update AxiosWithCredentials.js

--- src/hooks/AxiosWithCredentials.js
+++ src/hooks/AxiosWithCredentials.js
@@ -4,6 +4,6 @@ import secureLocalStorage from  "react-secure-storage";
 const ACCESS_TOKEN_KEY = "access_token";
 const REFRESH_TOKEN_KEY = "refresh_token";
 
-const axiosInstance = axios.create({ baseURL: process.env.REACT_APP_AUTH_API });
+const axiosWithCredentials = axios.create({ baseURL: process.env.REACT_APP_AUTH_API });
 
-export default axiosInstance;
+export default axiosWithCredentials;
