ede60ab29a9873cc941812183aa6c3ca84707fc8
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-27 19:08:39 +0100

    Update CardDeleteAccount.js

--- src/components/cards/users/CardDeleteAccount.js
+++ src/components/cards/users/CardDeleteAccount.js
@@ -5,26 +5,23 @@ export default function CardDeleteAccount(props) {
 
   return (
     <>
-      <Card.Body id={id} className="card">
-        <div className="d-flex align-items-center mb-sm-0 mb-4">
-          <div className="w-50">
-            <h5>Delete Account</h5>
-            <p className="text-sm mb-0">Once you delete your account, there is no going back. Please be certain.</p>
+      <Card id={id} className="mt-4">
+        <Card.Body>
+          <div className="d-flex align-items-center mb-sm-0 mb-4">
+            <div className="w-50">
+              <h5>Delete Account</h5>
+              <p className="text-sm mb-0">Once you delete your account, there is no going back. Please be certain.</p>
+            </div>
+            <div className="w-50 text-end">
+              <Button 
+                className="btn-outline-secondary mb-3 mb-md-0 ms-auto"
+              >
+                Deactivate
+              </Button>
+            </div>
           </div>
-          <div className="w-50 text-end">
-            <Button 
-              className="btn-outline-secondary mb-3 mb-md-0 ms-auto"
-            >
-              Deactivate
-            </Button>
-            <Button 
-              className="bg-gradient-danger mb-0 ms-2"
-            >
-              Delete Account
-            </Button>
-          </div>
-        </div>
-      </Card.Body>
+        </Card.Body>
+      </Card>
     </>
   );
 }
