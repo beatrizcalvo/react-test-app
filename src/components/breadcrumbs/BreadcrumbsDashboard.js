539ebee762e97885fb90706c0f55e5d2d253658c
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-27 10:40:54 +0100

    Update BreadcrumbsDashboard.js

--- src/components/breadcrumbs/BreadcrumbsDashboard.js
+++ src/components/breadcrumbs/BreadcrumbsDashboard.js
@@ -36,8 +36,8 @@ export default function BreadcrumbsDashboard(props) {
           breadcrumbsLinks.map((breadcrumb, index) => {
             const isActive = breadcrumb.name === getCurrentPageName();
             const getLinkContent = (breadcrumb.name === "Dashboard" && index === 0) 
-              ? (<i className="fa-solid fa-house" />) 
-              : ({breadcrumb.name});
+              ? return (<i className="fa-solid fa-house" />) 
+              : return ({breadcrumb.name});
             
             return (
               <li className={ "breadcrumb-item text-sm" + (isActive ? " active" : "") }>
