626c8ca3046166711f7d3a323bf582cb1a2613f1
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-27 18:53:08 +0100

    Update UsersService.js

--- src/services/UsersService.js
+++ src/services/UsersService.js
@@ -4,9 +4,11 @@ import axiosWithCredentials from "../hooks/AxiosWithCredentials";
 const axiosClient = axiosWithCredentials(process.env.REACT_APP_AUTH_API);
 
 const getCurrentUser = () => { return axiosClient.get("/users/me") };
+const deleteCurrentUser = () => { return axiosClient.delete("/users/me") };
 
 const UsersService = { 
-  getCurrentUser
+  getCurrentUser,
+  deleteCurrentUser
 };
 
 export default UsersService;
