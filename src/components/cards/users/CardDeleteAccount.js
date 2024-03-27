2b4f9510da4c04b79e0a233eb787798ac585b057
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-27 18:44:31 +0100

    Update CardDeleteAccount.js

--- src/components/cards/users/CardDeleteAccount.js
+++ src/components/cards/users/CardDeleteAccount.js
@@ -1,9 +1,20 @@
+import { Button } from "react-bootstrap";
+
 export default function CardDeleteAccount(props) {
   const { id } = props;
 
   return (
     <>
       <Card.Body id={id} className="card">
+        <div className="d-flex align-items-center mb-sm-0 mb-4">
+          <div className="w-50">
+            <h5>Delete Account</h5>
+            <p className="text-sm mb-0">Once you delete your account, there is no going back. Please be certain.</p>
+          </div>
+          <div className="w-50 text-end">
+    
+          </div>
+        </div>
       </Card.Body>
     </>
   );
