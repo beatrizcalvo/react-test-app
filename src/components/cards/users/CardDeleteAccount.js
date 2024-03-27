f25db06cecea8e1a5f70304a44148abe7ba6f140
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-03-27 18:50:07 +0100

    Update CardDeleteAccount.js

--- src/components/cards/users/CardDeleteAccount.js
+++ src/components/cards/users/CardDeleteAccount.js
@@ -12,10 +12,14 @@ export default function CardDeleteAccount(props) {
             <p className="text-sm mb-0">Once you delete your account, there is no going back. Please be certain.</p>
           </div>
           <div className="w-50 text-end">
-            <Button className="btn-outline-secondary mb-3 mb-md-0 ms-auto">
+            <Button 
+              className="btn-outline-secondary mb-3 mb-md-0 ms-auto"
+            >
               Deactivate
             </Button>
-            <Button className="bg-gradient-danger mb-0 ms-2">
+            <Button 
+              className="bg-gradient-danger mb-0 ms-2"
+            >
               Delete Account
             </Button>
           </div>
