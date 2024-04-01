2da0a4339135e8adceebb0622e6a89aedfbd0b17
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-04-01 16:42:45 +0200

    Update LoginForm.js

--- src/components/forms/LoginForm.js
+++ src/components/forms/LoginForm.js
@@ -72,7 +72,7 @@ export default function LoginForm(props) {
           variant="primary" 
           type="submit" 
           className="btn btn-lg bg-gradient-primary btn-lg w-100 mt-4 mb-0" 
-          textButton="Submit" 
+          titleButton="Submit" 
           isLoading={loadingAuth}
         />        
       </Form>
