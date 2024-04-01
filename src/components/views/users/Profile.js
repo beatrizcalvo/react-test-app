01e88716b2d609d666b1e73bdb03eb3ec0a48f59
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-04-01 18:43:38 +0200

    Update Profile.js

--- src/components/views/users/Profile.js
+++ src/components/views/users/Profile.js
@@ -3,6 +3,7 @@ import { Container, Row, Col, Card } from "react-bootstrap";
 
 import NavbarUserProfile from "../../navbars/NavbarUserProfile";
 import CardProfile from "../../cards/users/CardProfile";
+import CardBasicData from "../../cards/users/CardBasicData";
 import CardDeleteAccount from "../../cards/users/CardDeleteAccount";
 
 export default function Profile(props) {  
@@ -11,7 +12,7 @@ export default function Profile(props) {
   // Links config for profile navbar
   const navbarLinks = [
     { href: "#profile-info", title: "Profile", icon: "fa-solid fa-user", component: CardProfile },
-    { href: "#basic-info", title: "Basic Info", icon: "fa-regular fa-id-card" },
+    { href: "#basic-info", title: "Basic Info", icon: "fa-regular fa-id-card", component: CardBasicData },
     { href: "#change-password", title: "Change Password", icon: "fa-solid fa-lock" },
     { href: "#delete-account", title: "Delete Account", icon: "fa-solid fa-trash", component: CardDeleteAccount }
   ];
