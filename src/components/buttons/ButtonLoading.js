b0bec0b689b93d3b177e36450b1d1f39332a89aa
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-04-01 16:34:37 +0200

    Create ButtonLoading.js

---
+++ src/components/buttons/ButtonLoading.js
@@ -0,0 +1,31 @@
+import { Button, Spinner } from "react-bootstrap";
+
+export default function ButtonLoading(props) {
+  const { variant, type, className, text, isLoading } = props;
+  
+  return (
+    <>
+      <Button
+        variant={variant}
+        type={type}
+        {...(!!className ? { className: className } : {})}
+        disabled={isLoading}
+      >
+        {isLoading ? (
+          <>
+            <Spinner
+              as="span"
+              animation="border"
+              size="sm"
+              role="status"
+              aria-hidden="true"
+            />{" "}
+            Loading...
+          </>
+        ) : (
+          {text}
+        )}
+      </Button>
+    </>
+  );
+}
