071e9a96e38a9cf0c5a51ffb39f26cdc8c606f67
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-04-02 12:30:19 +0200

    Create UserInfoForm.js

---
+++ src/components/forms/UserInfoForm.js
@@ -0,0 +1,16 @@
+import { Row, Col } from "react-bootstrap";
+
+export default function UserInfoForm(props) {
+  return (
+    <>
+      <Row>
+        <Col className="col-4">
+        </Col>
+        <Col className="col-4">
+        </Col>
+        <Col className="col-4">
+        </Col>
+      </Row>
+    </>
+  );
+}
