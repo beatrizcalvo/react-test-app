d0b0dba63a2dec5ef46def610d914a02843debe9
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-04-02 17:10:42 +0200

    Update UserInfoForm.js

--- src/components/forms/users/UserInfoForm.js
+++ src/components/forms/users/UserInfoForm.js
@@ -86,12 +86,13 @@ export default function UserInfoForm(props) {
           <Col className="col-4">
             <Form.Group className="input-group input-group-static">
               <label className="font-weight-bold">Gender:</label>
-              <Form.Select aria-label="Default select example">
-                <option>Open this select menu</option>
-                <option value="1">One</option>
-                <option value="2">Two</option>
-                <option value="3">Three</option>
-              </Form.Select>
+              <div className="choices">
+                <Form.Select>
+                  <option value="1">Female</option>
+                  <option value="2">Male</option>
+                  <option value="3">Other</option>
+                </Form.Select>
+              </choices>
             </Form.Group>
           </Col>
           <Col className="col-8">
