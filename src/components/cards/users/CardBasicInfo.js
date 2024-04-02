263dc995497941d4fc2f7f6d2ad85cd671c33575
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-04-02 11:00:22 +0200

    Update CardBasicInfo.js

--- src/components/cards/users/CardBasicInfo.js
+++ src/components/cards/users/CardBasicInfo.js
@@ -1,4 +1,4 @@
-import { Card } from "react-bootstrap";
+import { Card, Row } from "react-bootstrap";
 
 export default function CardBasicInfo(props) {
   const { id } = props;
@@ -8,8 +8,12 @@ export default function CardBasicInfo(props) {
       <Card id={id} className="mt-4">
         <Card.Header>
           <h5>Basic Info</h5>
+          <i className="fas fa-user-edit text-secondary text-sm" />
         </Card.Header>
         <Card.Body className="pt-0">
+          <Row>
+            
+          </Row>
         </Card.Body>
       </Card>
     </>
