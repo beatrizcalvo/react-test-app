21f24721f747f06c96d6c8d746ce9a6b943adc6c
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-04-02 13:26:18 +0200

    Update UserInfoForm.js

--- src/components/forms/users/UserInfoForm.js
+++ src/components/forms/users/UserInfoForm.js
@@ -1,18 +1,28 @@
-import { Row, Col } from "react-bootstrap";
+import { Row, Col, Form } from "react-bootstrap";
 
 export default function UserInfoForm(props) {
   const { user, readOnly } = props;
   
   return (
     <>
-      <Row>
-        <Col className="col-4">
-        </Col>
-        <Col className="col-4">
-        </Col>
-        <Col className="col-4">
-        </Col>
-      </Row>
+      <Form>
+        <Row>
+          <Col className="col-4">
+            <Form.Group className="input-group input-group-static">
+              <Form.Label>First Name</Form.Label>
+              <Form.Control 
+                type="text" 
+                placeholder="First Name..."
+                plaintext readOnly defaultValue={user.firstName}
+              />
+            </Form.Group>
+          </Col>
+          <Col className="col-4">
+          </Col>
+          <Col className="col-4">
+          </Col>
+        </Row>
+      </Form>
     </>
   );
 }
