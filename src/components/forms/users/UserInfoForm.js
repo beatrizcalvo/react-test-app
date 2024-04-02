32abc99231ae4d9f4c445268ad27bf303acca518
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-04-02 17:48:35 +0200

    Update UserInfoForm.js

--- src/components/forms/users/UserInfoForm.js
+++ src/components/forms/users/UserInfoForm.js
@@ -94,6 +94,12 @@ export default function UserInfoForm(props) {
                   <option value="2">Two</option>
                   <option value="3">Three</option>
               </datalist>
+              <Form.Select aria-label="Default select example">
+      <option>Open this select menu</option>
+      <option value="1">One</option>
+      <option value="2">Two</option>
+      <option value="3">Three</option>
+    </Form.Select>
             </Form.Group>
           </Col>
           <Col className="col-8">
