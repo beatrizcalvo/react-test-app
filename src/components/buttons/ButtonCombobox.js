5f8f1c5ed4beb071c2ae73f20eb9dae2abe5cde2
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-04-03 17:45:00 +0200

    Update ButtonCombobox.js

--- src/components/buttons/ButtonCombobox.js
+++ src/components/buttons/ButtonCombobox.js
@@ -35,6 +35,7 @@ export default function ButtonCombobox(props) {
 	role="combobox"
         data-type={readOnly ? "none" : "select-one"} 
         aria-expanded={isOpen}
+	{...(!readOnly ? { onBlur: () => setIsOpen(false) } : {})}
       >
         <div 
           {...(!readOnly ? { className: "choices__inner" } : {})}
