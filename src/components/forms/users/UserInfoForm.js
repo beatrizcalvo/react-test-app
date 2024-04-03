baea719cad30edba24fdf4c2787826e5a2f44d4f
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-04-03 11:30:35 +0200

    Update UserInfoForm.js

--- src/components/forms/users/UserInfoForm.js
+++ src/components/forms/users/UserInfoForm.js
@@ -38,8 +38,8 @@ export default function UserInfoForm(props) {
             <Form.Group className="input-group input-group-static">
               <label for="firstName" className="font-weight-bold">First Name:</label>
               <Form.Control 
-                type="text" 
                 id="firstName"
+                type="text" 
                 {...(!readOnly ? { placeholder: "First Name..." } : {})}
                 {...register("firstName", inputValidations.firstName)}
                 {...(readOnly ? { readOnly: true, plaintext: true, className: "text-sm" } : {})}
@@ -55,8 +55,8 @@ export default function UserInfoForm(props) {
             <Form.Group className="input-group input-group-static">
               <label for="lastName" className="font-weight-bold">Last Name:</label>
               <Form.Control 
-                type="text" 
                 id="lastName"
+                type="text" 
                 {...(!readOnly ? { placeholder: "Last Name..." } : {})}
                 {...register("lastName", inputValidations.lastName)}
                 {...(readOnly ? { readOnly: true, plaintext: true, className: "text-sm" } : {})}
@@ -72,8 +72,8 @@ export default function UserInfoForm(props) {
             <Form.Group className="input-group input-group-static">
               <label for="secondLastName" className="font-weight-bold">Second Last Name:</label>
               <Form.Control 
-                type="text" 
                 id="secondLastName"
+                type="text" 
                 {...(!readOnly ? { placeholder: "Second Last Name..." } : {})}
                 {...register("secondLastName", inputValidations.secondLastName)}
                 {...(readOnly ? { readOnly: true, plaintext: true, className: "text-sm" } : {})}
@@ -91,11 +91,14 @@ export default function UserInfoForm(props) {
             <Form.Group className="input-group input-group-static">
               <label className="font-weight-bold">Gender:</label>
               <div className="choices" data-type="select-one">
-                <Form.Select>
-                  {["One", "Two", "Three"].map(option => {
-                    <option value={option}>{option}</option>
+                <Form.Control
+                  id="gender"
+                  as="select"
+                >
+                  {["Female", "Male"].map(item => {
+                    return (<option key={item} value={item}>{item}</option>);
                   })}
-                </Form.Select>
+                </Form.Control>
               </div>
             </Form.Group>
           </Col>
