64a452dba673121fc9dd91c8094b1105003192f9
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-04-03 14:00:16 +0200

    Update ButtonCombobox.js

--- src/components/buttons/ButtonCombobox.js
+++ src/components/buttons/ButtonCombobox.js
@@ -1,5 +1,6 @@
 import classNames from "classnames";
 import { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export default function ButtonCombobox(props) {
   const [isOpen, setIsOpen ] = useState(false);
@@ -14,15 +15,14 @@ export default function ButtonCombobox(props) {
         aria-expanded={isOpen}
       >
         <div className="choices__inner" onClick={() => alert("Pulsado")}>
-          <div className={classNames({ "choices__list choices__list--single": !readOnly })}>
-	    <div 
-	      className={classNames({
-		"choices__item choices__item--selectable": !readOnly, 
-		"text-sm form-control-plaintext": readOnly
-	      })}
-	    >
-	      English
-	    </div>
+          <div {...(!readOnly ? { className: "choices__list choices__list--single" } : {})}>
+	    <Form.Control
+	      id={id}
+	      type="text"
+	      readOnly=true
+	      {...(!readOnly ? { placeholder: "Select one..." } : {})}
+	      {...(readOnly ? { plaintext: true, className: "text-sm" } : {})}
+	    />
 	  </div>
         </div>
         <div className="choices__list choices__list--dropdown" aria-expanded={isOpen}>
