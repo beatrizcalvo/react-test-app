c654c4e08bd3bf3a29817e65affabc8a9daab6a3
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-04-03 13:21:33 +0200

    Update ButtonCombobox.js

--- src/components/buttons/ButtonCombobox.js
+++ src/components/buttons/ButtonCombobox.js
@@ -6,8 +6,20 @@ export default function ButtonCombobox(props) {
   
   return (
     <>
-      <div className="choices" data-type="select-one">
-        
+      <div 
+        className="choices" 
+        data-type={readOnly ? "none" : "select-one"} 
+        aria-expanded={isOpen}
+      >
+        <div class="choices__inner">
+          <div class="choices__list choices__list--single">
+			      <div class="choices__item choices__item--selectable">
+				      English
+			      </div>
+		      </div>
+        </div>
+        <div class="choices__list choices__list--dropdown" aria-expanded={isOpen}>
+        </div>
       </div>
     </>
   );
