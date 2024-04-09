938249d4c2fa5778f7f81a04d6a359dc4c64f8c1
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-04-09 17:21:23 +0200

    Update DatePickerWithHeader.js

--- src/components/inputs/DatePickerWithHeader.js
+++ src/components/inputs/DatePickerWithHeader.js
@@ -4,7 +4,7 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
 const CustomInput = forwardRef((props: any, ref) => {
-  return <button className="example-custom-input" onClick={onClick} ref={ref}>{value}</button>;
+  return <button className="example-custom-input" onClick={props.onClick} ref={ref}>{props.value}</button>;
 });
 
 export default function DatePickerWithHeader(props) {
