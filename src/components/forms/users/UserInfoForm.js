e4cb72d510f17f0cd86fc6bff6ce358f79193a9c
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-04-09 21:14:53 +0200

    Update UserInfoForm.js

--- src/components/forms/users/UserInfoForm.js
+++ src/components/forms/users/UserInfoForm.js
@@ -67,12 +67,12 @@ export default function UserInfoForm(props) {
       .then(response => {
         alert(JSON.stringify(response));
         const itemsList = [];
-        //response.data.nationalities.map(nationality => {
-          //itemsList.push({
-            //code: nationality.nationalityCode,
-            //description: nationality.nationalityDescription
-          //});
-        //});
+        response.data.nationalities?.map(nationality => {
+          itemsList.push({
+            code: nationality.nationalityCode,
+            description: nationality.nationalityDescription
+          });
+        });
         setNationalitiesList(itemsList);
       })
       .catch(error => alert(error));
