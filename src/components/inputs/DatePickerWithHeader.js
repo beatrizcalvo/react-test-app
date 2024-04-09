383778e8329737d4346de486094bb04ca5dbc1bf
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-04-09 17:31:55 +0200

    Update DatePickerWithHeader.js

--- src/components/inputs/DatePickerWithHeader.js
+++ src/components/inputs/DatePickerWithHeader.js
@@ -1,10 +1,20 @@
 import { useState, useRef, forwardRef } from "react";
+import { Form } from "react-bootstrap";
 import DatePicker from "react-datepicker";
 
 import "react-datepicker/dist/react-datepicker.css";
 
-const CustomInput = forwardRef((props: any, ref) => {
-  return <button className="example-custom-input" onClick={props.onClick} ref={ref}>{props.value}</button>;
+const CustomInput = forwardRef((props, ref) => {
+  const { id } = props;
+  
+  return (
+    <Form.Control 
+      id={id}
+      type="text"
+      {...(!readOnly ? { placeholder: "Select a date..." } : {})}
+      {...(readOnly ? { readOnly: true, plaintext: true, className: "text-sm" } : {})}
+    />
+  );
 });
 
 export default function DatePickerWithHeader(props) {
