54b86a02721ff39ec98ee6fe86d0250d9ca20cc7
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-04-09 15:53:43 +0200

    Update DatePickerWithHeader.js

--- src/components/inputs/DatePickerWithHeader.js
+++ src/components/inputs/DatePickerWithHeader.js
@@ -1,3 +1,12 @@
+import { useState } from "react";
+import DatePicker from "react-datepicker";
+
 export default function DatePickerWithHeader(props) {
+  const [selectedDate, setSelectedDate] = useState(new Date());
   
+  return (
+    <>
+      <DatePicker selected={selectedDate} onChange={(date) => setSelectedDate(date)} />
+    </>
+  );
 }
