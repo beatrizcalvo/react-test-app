7c601d26acc505ca5be87f6571050ad388168f37
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-04-10 14:06:47 +0200

    Update UserInfoForm.js

--- src/components/forms/users/UserInfoForm.js
+++ src/components/forms/users/UserInfoForm.js
@@ -95,9 +95,6 @@ export default function UserInfoForm(props) {
     console.log(JSON.stringify(dirtyFields));
     const dataUpdated = getDirtyValues(dirtyFields, data);
     console.log(JSON.stringify(dataUpdated));
-    const object1 = { a: 1, b: 2, c: 3 };
-    const object2 = Object.fromEntries(Object.entries(object1).map(([key, val]) => [key, val * 2]));
-    console.log(object2);
   };
   
   return (
