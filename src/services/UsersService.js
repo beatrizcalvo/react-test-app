d66949f4f20bd214bd858022bc72f0e0fcada7ae
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-04-10 16:54:48 +0200

    Update UsersService.js

--- src/services/UsersService.js
+++ src/services/UsersService.js
@@ -5,7 +5,18 @@ const axiosClient = axiosWithCredentials(process.env.REACT_APP_AUTH_API);
 
 const getCurrentUser = () => { return axiosClient.get("/users/me") };
 const deleteCurrentUser = () => { return axiosClient.delete("/users/me") };
+const deactivateCurrentUser = () => { return axiosClient.patch("/users/me", { active: false }) };
+
 const updateCurrentUser = (data) => { return axiosClient.patch("/users/me", {
+    ...((!!data.firstName || !!data.lastName || !!data.secondLastName) && {
+      person: {
+        personName: {
+          firstName: data.firstName,
+          lastName: data.lastName,
+          secondLastName: data.secondLastName
+        }  
+      }
+    })
     gender: data.gender?.description,
     ...(!!data.nationality?.code && {
       firstNationality: {
@@ -13,8 +24,7 @@ const updateCurrentUser = (data) => { return axiosClient.patch("/users/me", {
       }
     })
   }
-})};
-const deactivateCurrentUser = () => { return axiosClient.patch("/users/me", { active: false }) };
+)};
 
 const getNationalities = () => { return axiosClient.get("/nationalities") };
 
