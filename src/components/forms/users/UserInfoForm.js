4e6330ea072ac78aa752e2f05bda3795f716f01d
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-04-10 13:51:43 +0200

    Update UserInfoForm.js

--- src/components/forms/users/UserInfoForm.js
+++ src/components/forms/users/UserInfoForm.js
@@ -90,9 +90,6 @@ export default function UserInfoForm(props) {
     const object1 = { a: 1, b: 2, c: 3 };
     const object2 = Object.fromEntries(Object.entries(object1).map(([key, val]) => [key, val * 2]));
     console.log(object2);
-);
-
-console.log(object2);
   };
   
   return (
