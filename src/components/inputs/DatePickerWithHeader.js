13202e03920e590a47680789347ac8e20cb0b0d7
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-04-10 12:18:29 +0200

    Update DatePickerWithHeader.js

--- src/components/inputs/DatePickerWithHeader.js
+++ src/components/inputs/DatePickerWithHeader.js
@@ -6,22 +6,24 @@ import { useFormContext } from "react-hook-form";
 import "react-datepicker/dist/react-datepicker.css";
 
 const CustomInput = forwardRef((props, ref) => {
-  const { value, onClick, onChange } = props;
+  const { id, value, onClick, onChange } = props;
 
   console.log(props);
   
   return (
     <Form.Control
+      id={id}
       type="text"
-      value={value}
-      onChange={(e) => onChange(e.target.value)}
+      readOnly="true"
+      //value={value}
+      //onChange={(e) => onChange(e.target.value)}
       onClick={onClick}
     />
   );
 });
 
 export default function DatePickerWithHeader({ id, readOnly }) {
-  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [selectedDate, setSelectedDate] = useState(undefined);
   
   return (
     <>
