f5c899e1375272588acc06cf8daf345a85a41c47
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-04-15 09:53:46 +0200

    Update UserInfoForm.js

--- src/components/forms/users/UserInfoForm.js
+++ src/components/forms/users/UserInfoForm.js
@@ -86,6 +86,7 @@ export default function UserInfoForm(props) {
     // If *any* item in an array was modified, the entire array must be submitted, because there's no
     // way to indicate "placeholders" for unchanged elements. `dirtyFields` is `true` for leaves.
     if (dirtyFields === true || Array.isArray(dirtyFields)) {
+      console.log(typeof allValules[dirtyFields]);
       return allValues;
     }
     return Object.fromEntries(Object.keys(dirtyFields).map((key) => [key, getDirtyValues(dirtyFields[key], allValues[key])]));
