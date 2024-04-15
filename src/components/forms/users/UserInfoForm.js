cf76efe90c500870da31b6733a01c9442134546b
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-04-15 11:39:56 +0200

    Update UserInfoForm.js

--- src/components/forms/users/UserInfoForm.js
+++ src/components/forms/users/UserInfoForm.js
@@ -87,7 +87,7 @@ export default function UserInfoForm(props) {
     // way to indicate "placeholders" for unchanged elements. `dirtyFields` is `true` for leaves.
     if (dirtyFields === true || Array.isArray(dirtyFields)) {
       // Format date to yyyy-MM-dd
-      if (allValues[dirtyFields].match("/^(\d{1,2})\/(\d{1,2})\/(\d{4})$/")) console.log("dirtyFields: " + dirtyFields + " | value: " + allValues);
+      console.log("dirtyFields: " + dirtyFields + " | value: " + allValues);
       return allValues;
     }
     return Object.fromEntries(Object.keys(dirtyFields).map((key) => [key, getDirtyValues(dirtyFields[key], allValues[key])]));
