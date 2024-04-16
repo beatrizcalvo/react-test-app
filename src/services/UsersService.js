3aee6565191172da19d53d78c75437bebd70c541
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-04-16 08:54:36 +0200

    Update UsersService.js

--- src/services/UsersService.js
+++ src/services/UsersService.js
@@ -11,7 +11,7 @@ const updateCurrentUser = (data) => { return axiosClient.patch("/users/me", {
   ...((data.firstName || data.lastName || data.secondLastName || data.gender?.description || data.birthDate 
        || data.nationality?.code) && {
     person: {
-      ...((data.firstName) && {
+      ...((data.firstName || data.lastName || data.secondLastName) && {
         personName: {
           firstName: data.firstName,
           lastName: data.lastName,
