c1a37e2a38043ebe5ad9c6572783b42c6f24259c
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-04-16 17:56:40 +0200

    Update DatePickerWithHeader.js

--- src/components/inputs/DatePickerWithHeader.js
+++ src/components/inputs/DatePickerWithHeader.js
@@ -60,7 +60,7 @@ export default function DatePickerWithHeader({ id, readOnly, inputValidations })
             maxDate={subYears(new Date(), 18)}
             selected={value}
             onChange={(date) => onChange(date)}
-            renderCustomHeader={CustomHeader}
+            renderCustomHeader={() => CustomHeader}
             customInput={<CustomInput />}
           />
         )}
