afc59fd0eecd61716599b58b53e4e23614e474c3
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-04-18 10:28:19 +0200

    Update DatePickerWithHeader.js

--- src/components/inputs/DatePickerWithHeader.js
+++ src/components/inputs/DatePickerWithHeader.js
@@ -21,7 +21,7 @@ const getMonthList = (locales?: string | string[], format: "long" | "short" = "l
 const CustomHeader = ({ date, increaseMonth }) => {
   const [ showMonthSelect, setShowMonthSelect ] = useState(false);
   const [ showYearSelect, setShowYearSelect ] = useState(false);
-  const months = getMonthList("en");
+  const months = getMonthList("en").map(month => ({ code: month, description: month }));
 
   var arr1 = [
  {"user":"dan","liked":"yes","age":"22"},
