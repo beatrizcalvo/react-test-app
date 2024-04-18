73ad3a3f5153362a5b5a341af4659395ac391c77
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-04-18 14:09:21 +0200

    Update DatePickerWithHeader.js

--- src/components/inputs/DatePickerWithHeader.js
+++ src/components/inputs/DatePickerWithHeader.js
@@ -17,8 +17,7 @@ const getMonthList = (locales?: string | string[], format: "long" | "short" = "l
 };
 
 const CustomHeaderToggle = forwardRef(({ children, onClick }, ref) => (
-  <a href="" ref={ref}
-    onClick={(e) => {
+  <a href="" ref={ref} onClick={(e) => {
       e.preventDefault();
       onClick(e);
     }}
@@ -28,12 +27,40 @@ const CustomHeaderToggle = forwardRef(({ children, onClick }, ref) => (
   </a>
 ));
 
+const CustomHeaderMenu = forwardRef(
+  ({ children, style, className, 'aria-labelledby': labeledBy }, ref) => {
+    const [value, setValue] = useState('');
+
+    return (
+      <div
+        ref={ref}
+        style={style}
+        className={className}
+        aria-labelledby={labeledBy}
+      >
+        <Form.Control
+          autoFocus
+          className="mx-3 my-2 w-auto"
+          placeholder="Type to filter..."
+          onChange={(e) => setValue(e.target.value)}
+          value={value}
+        />
+        <ul className="list-unstyled">
+          {React.Children.toArray(children).filter(
+            (child) =>
+              !value || child.props.children.toLowerCase().startsWith(value),
+          )}
+        </ul>
+      </div>
+    );
+  },
+);
+
 const CustomHeader = ({ date }) => {
-  
   return (  
     <div className="d-flex justify-content-center">
       <Dropdown>
-        <Dropdown.Toggle as={CustomToggle} id="month-year-select">
+        <Dropdown.Toggle as={CustomHeaderToggle} id="month-year-select">
           Custom toggle
         </Dropdown.Toggle>
       </Dropdown>
