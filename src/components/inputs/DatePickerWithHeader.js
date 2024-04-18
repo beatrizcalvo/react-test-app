30e2df8f2b3b8a5d0c0098bbb5b88de03c35c06e
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-04-18 15:57:30 +0200

    Update DatePickerWithHeader.js

--- src/components/inputs/DatePickerWithHeader.js
+++ src/components/inputs/DatePickerWithHeader.js
@@ -63,6 +63,14 @@ const CustomHeader = ({ date }) => {
         <Dropdown.Toggle as={CustomHeaderToggle} id="month-year-select">
           Custom toggle
         </Dropdown.Toggle>
+        <Dropdown.Menu as={CustomMenu}>
+      <Dropdown.Item eventKey="1">Red</Dropdown.Item>
+      <Dropdown.Item eventKey="2">Blue</Dropdown.Item>
+      <Dropdown.Item eventKey="3" active>
+        Orange
+      </Dropdown.Item>
+      <Dropdown.Item eventKey="1">Red-Orange</Dropdown.Item>
+    </Dropdown.Menu>
       </Dropdown>
     </div>
   );
