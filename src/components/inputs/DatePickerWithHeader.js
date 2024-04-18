a476dd8696e4889bff5a69ad9327af954caa6ca9
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-04-18 12:33:38 +0200

    Update DatePickerWithHeader.js

--- src/components/inputs/DatePickerWithHeader.js
+++ src/components/inputs/DatePickerWithHeader.js
@@ -29,9 +29,7 @@ const CustomHeader = ({ date }) => {
         <option value="3">Three</option>
       </Form.Select>
       <Form.Select>
-        <option value="1">One</option>
-        <option value="2">Two</option>
-        <option value="3">Three</option>
+        {months.map((month) => ( <option key={month} value={month}>{month}</option> ))}
       </Form.Select>
     </div>
   );
