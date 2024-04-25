55e122b28edad5465f7ba051cf8087f822536377
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-04-25 11:45:29 +0200

    Update DatePickerWithHeader.js

--- src/components/inputs/DatePickerWithHeader.js
+++ src/components/inputs/DatePickerWithHeader.js
@@ -21,15 +21,15 @@ const CustomHeader = ({ date }) => {
   return (  
     <div className="d-flex m-2">
       <span className="datepicker-prev-month">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 17 17">
-          <g></g>
-          <path d="M5.207 8.471l7.146 7.147-0.707 0.707-7.853-7.854 7.854-7.853 0.707 0.707-7.147 7.146z"></path>
-        </svg>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 17 17"><g></g><path d="M5.207 8.471l7.146 7.147-0.707 0.707-7.853-7.854 7.854-7.853 0.707 0.707-7.147 7.146z"></path></svg>
       </span>
       <div className="datepicker-month">
+        <div className="datepicker-current-month">
+          
+        </div>
       </div>
       <span className="datepicker-next-month">
-        
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 17 17"><g></g><path d="M13.207 8.472l-7.854 7.854-0.707-0.707 7.146-7.146-7.146-7.148 0.707-0.707 7.854 7.854z"></path></svg>
       </span>
     </div>
   );
