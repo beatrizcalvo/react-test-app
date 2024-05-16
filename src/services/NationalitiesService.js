4c910bc7daa343c015787d4713ba7d607022d63e
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-05-16 22:56:57 +0200

    Update NationalitiesService.js

--- src/services/NationalitiesService.js
+++ src/services/NationalitiesService.js
@@ -2,3 +2,5 @@ import axiosWithCredentials from "../hooks/AxiosWithCredentials";
 
 const axiosClient = axiosWithCredentials(process.env.REACT_APP_API_URL);
 
+const getNationalities = () => { return axiosClient.get("/nationalities") };
+
