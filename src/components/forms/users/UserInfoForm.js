d6edfa7ba859d26073013449375e407404a53d95
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-05-19 11:08:54 +0200

    Update UserInfoForm.js

--- src/components/forms/users/UserInfoForm.js
+++ src/components/forms/users/UserInfoForm.js
@@ -97,6 +97,7 @@ export default function UserInfoForm(props) {
 
   // Get only fields updated and submit form
   const onSubmit = (data) => {
+    alert(JSON.stringify(errors));
     const dataUpdated = getDirtyValues(dirtyFields, data);
     handleUpdateProfile(dataUpdated);
   };
