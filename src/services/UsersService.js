887a58303dca03150a87d71a822ed06c075876c0
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-05-23 00:07:59 +0200

    Update UsersService.js

--- src/services/UsersService.js
+++ src/services/UsersService.js
@@ -27,7 +27,7 @@ const updateCurrentUser = (data) => { return axiosClient.patch("/users/me", {
     },
     contactPoint: {
       postalAddress: {
-        addressLines: ["1234567890"]
+        addressLines: []
       }
     }
   })
