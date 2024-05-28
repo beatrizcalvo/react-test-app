5855765e689a15e1e4e7f32cbb2c3e2051ba86e2
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-05-28 13:03:55 +0200

    Update UsersService.js

--- src/services/UsersService.js
+++ src/services/UsersService.js
@@ -31,8 +31,8 @@ const updateCurrentUser = (data) => { return axiosClient.patch("/users/me", {
       postalAddress: {
         ...((data.addressLine1 || data.addressLine2) && {
           addressLines: [
-            ...(data.addressLine1 $$ { data.addressLine1 }),
-            ...(data.addressLine2 $$ { data.addressLine2 })
+            ...(data.addressLine1 && { data.addressLine1 }),
+            ...(data.addressLine2 && { data.addressLine2 })
           ]
         }),
         city: data.city,
