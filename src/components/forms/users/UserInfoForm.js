e422766f11fe6ce7ca8561a1af143aa9947462bc
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-06-07 12:40:48 +0200

    Update UserInfoForm.js

--- src/components/forms/users/UserInfoForm.js
+++ src/components/forms/users/UserInfoForm.js
@@ -131,7 +131,6 @@ export default function UserInfoForm(props) {
       dataUpdated.addressLine1 = dataUpdated.addressLine1 || data.addressLine1;
       dataUpdated.addressLine2 = dataUpdated.addressLine2 || data.addressLine2;
     }
-    console.log(JSON.stringify(dataUpdated));
     handleUpdateProfile(dataUpdated);
   };
   
