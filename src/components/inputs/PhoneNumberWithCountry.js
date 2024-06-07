a254a875ecf865a748600e4b1519dcb0e8fc307f
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-06-07 12:48:10 +0200

    Update PhoneNumberWithCountry.js

--- src/components/inputs/PhoneNumberWithCountry.js
+++ src/components/inputs/PhoneNumberWithCountry.js
@@ -1,3 +1,8 @@
+import { useFormContext, Controller } from "react-hook-form";
+import PhoneInput from "react-phone-number-input";
+
+import "react-phone-number-input/style.css";
+
 export default function PhoneNumberWithCountry({ id, readOnly, inputValidations }) {
   return (
     <>
