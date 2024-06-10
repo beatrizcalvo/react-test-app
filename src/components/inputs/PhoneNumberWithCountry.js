04e7efb7620aacd43cd3f31ad085cce2affc2ac5
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-06-10 17:25:39 +0200

    Update PhoneNumberWithCountry.js

--- src/components/inputs/PhoneNumberWithCountry.js
+++ src/components/inputs/PhoneNumberWithCountry.js
@@ -11,27 +11,23 @@ export default function PhoneNumberWithCountry({ id, readOnly, inputValidations,
   
   return (
     <>
-      { readOnly && (
-        <p>Prueba</p>
-        ) || (
-          <Controller 
-            name={id}
-            control={control}
-            rules={inputValidations}
-            render={({ field }) => (
-              <PhoneInput 
-                {...field}
-                name={field.name}
-                placeholder="Phone number..."
-                onChange={(value: string) => field.onChange(value)}
-                countries={countries}
-                international={true}
-                countryCallingCodeEditable={false}
-              />
-            )}
+      <Controller 
+        name={id}
+        control={control}
+        rules={inputValidations}
+        render={({ field }) => (
+          <PhoneInput 
+            {...field}
+            name={field.name}
+            placeholder="Phone number..."
+            onChange={(value: string) => field.onChange(value)}
+            countries={countries}
+            international={true}
+            countryCallingCodeEditable={false}
+            numberInputProps={ className: {"form-control"} }
           />
-        )
-      }
+        )}
+      />
     </>
   );
 }
