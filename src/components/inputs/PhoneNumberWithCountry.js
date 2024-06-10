12831b5b271c52a7841e4b77fb53131101ce01cb
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-06-10 17:00:55 +0200

    Update PhoneNumberWithCountry.js

--- src/components/inputs/PhoneNumberWithCountry.js
+++ src/components/inputs/PhoneNumberWithCountry.js
@@ -5,38 +5,33 @@ import PhoneInput from "react-phone-number-input";
 import "react-phone-number-input/style.css";
 
 export default function PhoneNumberWithCountry({ id, readOnly, inputValidations, countries }) {  
-  const { register, control } = useFormContext();
+  const { control } = useFormContext();
+
+  console.log(JSON.stringify(countries));
   
   return (
     <>
       { readOnly && (
-          <Form.Control 
-            id={id} 
-            type="text" 
-            {...register(id)}
-          />
+        
         ) || (
-          <Controller />
+          <Controller 
+            name={id}
+            control={control}
+            rules={inputValidations}
+            render={({ field }) => (
+              <PhoneInput 
+                {...field}
+                name={field.name}
+                placeholder="Phone number..."
+                onChange={(value: string) => field.onChange(value)}
+                countries={countries}
+                international={true}
+                countryCallingCodeEditable={false}
+              />
+            )}
+          />
         )
       }
-              
-      /*<Controller
-        name={id}
-        control={control} 
-        rules={inputValidations}
-        render={({ field }) => (
-          <PhoneInput 
-            {...field}
-            name={field.name}
-            placeholder="Phone number..." 
-            readOnly={readOnly}
-            onChange={(value: string) => field.onChange(value)}
-            countries={countries}
-            international={true}
-            countryCallingCodeEditable={false}
-          />
-        )}
-      />*/
     </>
   );
 }
