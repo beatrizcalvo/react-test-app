6e4392929cf100b430252ecfa735af0632b2bb13
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-06-10 11:26:28 +0200

    Update PhoneNumberWithCountry.js

--- src/components/inputs/PhoneNumberWithCountry.js
+++ src/components/inputs/PhoneNumberWithCountry.js
@@ -1,13 +1,21 @@
 import { useState } from "react";
+import { useFormContext, Controller } from "react-hook-form";
 import PhoneInput from "react-phone-number-input";
 
 import "react-phone-number-input/style.css";
 
 export default function PhoneNumberWithCountry({ id, readOnly, inputValidations }) {  
-  const [value, setValue] = useState()
+  const [value, setValue] = useState();
+  const { control } = useFormContext();
   
   return (
     <>
+      <Controller
+        name={id}
+        control={control} 
+        rules={inputValidations}
+        render={({ field: { name, value, onChange }, formState: { errors } }) => ()}
+      />
       <PhoneInput
         placeholder="Phone number..."
         readOnly={readOnly}
