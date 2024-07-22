15cfc8a2e9b8dc581882f1fbca0963a3a307dbef
beatrizcalvo <38405749+beatrizcalvo@users.noreply.github.com>
2024-07-22 11:09:55 +0200

    Update PhoneNumberWithCountry.js

--- src/components/inputs/PhoneNumberWithCountry.js
+++ src/components/inputs/PhoneNumberWithCountry.js
@@ -6,8 +6,6 @@ import "react-phone-number-input/style.css";
 
 export default function PhoneNumberWithCountry({ id, readOnly, inputValidations, countries }) {  
   const { control } = useFormContext();
-
-  console.log(JSON.stringify(countries));
   
   return (
     <>
@@ -22,7 +20,7 @@ export default function PhoneNumberWithCountry({ id, readOnly, inputValidations,
             readOnly={readOnly}
             placeholder="Phone number..."
             onChange={(value: string) => field.onChange(value)}
-            countries=['FR', 'ES']
+            countries={["FR", "ES"]}
             numberInputProps={{ className: (readOnly ? "text-sm form-control-plaintext" : "form-control") }}
           />
         )}
